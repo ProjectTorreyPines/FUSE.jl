9d2db49b6c13f2ccff5d8dd7d6a7f8e421e4979e
Orso Meneghini <orso82@gmail.com>
2021-10-10 14:17:57 -0700

    move no_Dual to utils

--- src/actors/solovev_equilibrium_actor.jl
+++ src/actors/solovev_equilibrium_actor.jl
@@ -50,15 +50,6 @@ function SolovevEquilibriumActor(equilibrium::IMAS.equilibrium, time::Real; qsta
     SolovevEquilibriumActor(equilibrium, time, S0, IMAS.equilibrium())
 end
 
-function no_Dual(x)
-    if typeof(x) <: ForwardDiff.Dual
-        x = x.value
-        return no_Dual(x)
-    else
-        return x
-    end
-end
-
 #= == =#
 # STEP #
 #= == =#
--- src/utils.jl
+++ src/utils.jl
@@ -1,1 +1,10 @@
 struct StopIteration <: Exception end
+
+function no_Dual(x)
+    if typeof(x) <: ForwardDiff.Dual
+        x = x.value
+        return no_Dual(x)
+    else
+        return x
+    end
+end
