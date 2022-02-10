c2f3101d4c1031574945d8db5056a29ec7aa08a2
Orso Meneghini <orso82@gmail.com>
2022-02-10 00:02:25 -0800

    some cleanup

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -20,7 +20,7 @@ include("parameters.jl")
 #= EXPORT =#
 #= ====== =#
 export init, step, finalize
-export IMAS, is_missing, @ddtime, @coords
+export IMAS, is_missing, @ddtime, @coords, constants, ±
 export fuse_parameters, plasma_parameters, physics_models
 
 end
