f7554ecb28e96bdb6609070e75c31e736aec82a6
Orso Meneghini <orso82@gmail.com>
2022-02-23 09:18:01 -0800

    Add all heating sources to ITER case

--- parameters/ITER.jl
+++ parameters/ITER.jl
@@ -23,7 +23,7 @@ function Parameters(::Type{Val{:ITER}}; init_from)
     par.pf_active.n_pf_coils_inside = 0
     par.pf_active.n_pf_coils_outside = 6
 
-    par.core_profiles.ne_ped = 7E19
+    par.core_profiles.ne_ped = 9E19
     par.core_profiles.n_peaking = 1.5
     par.core_profiles.T_shaping = 1.8
     par.core_profiles.w_ped = 0.08
@@ -32,7 +32,7 @@ function Parameters(::Type{Val{:ITER}}; init_from)
     par.core_profiles.bulk = :DT
     par.core_profiles.impurity = :Ne
 
-    par.nbi.beam_power = 16.7E6
+    par.nbi.beam_power = 16.7E6 * 2 + 10E6 * 2 + 1E6 * 24 # NBI+IC+EC
     par.nbi.beam_energy = 1000e3
 
     return par
