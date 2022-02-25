56f37c043949c4dedc8acbe6b6ad7f9a51665b88
Orso Meneghini <orso82@gmail.com>
2022-02-25 14:31:32 -0800

    load FPP case from STEP ODS

--- cases/FPP.jl
+++ cases/FPP.jl
@@ -1,11 +1,18 @@
-function Parameters(::Type{Val{:FPP}})
+function Parameters(::Type{Val{:FPP}}; init_from::Symbol)
     par = Parameters()
-    par.general.casename = "FPP"
-    par.general.init_from = :gasc
+    par.general.casename = "FPP_$(init_from)"
+    par.general.init_from = init_from
 
-    par.gasc.filename = joinpath(dirname(abspath(@__FILE__)), "..", "sample", "FPP_fBS_PBpR_scan.json")
-    par.gasc.case = 59
-    par.gasc.no_small_gaps = true
+    if init_from == :ods
+        par.ods.filename = joinpath(dirname(abspath(@__FILE__)), "..", "sample", "fpp_gasc_59_step.json")
+    else
+        par.gasc.filename = joinpath(dirname(abspath(@__FILE__)), "..", "sample", "FPP_fBS_PBpR_scan.json")
+        par.gasc.case = 59
+        par.gasc.no_small_gaps = true
+
+        par.core_profiles.rot_core = 0.0
+        par.core_profiles.bulk = :DT
+    end
 
     par.build.is_nuclear_facility = true
     par.build.symmetric = false
@@ -13,10 +20,6 @@ function Parameters(::Type{Val{:FPP}})
     par.pf_active.n_oh_coils = 6
     par.pf_active.n_pf_coils_inside = 0
     par.pf_active.n_pf_coils_outside = 6
-
-    par.core_profiles.rot_core = 0.0
-
-    par.core_profiles.bulk = :DT
 
     return par
 end
