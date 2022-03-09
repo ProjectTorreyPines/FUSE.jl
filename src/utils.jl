1df87144105a8cdbd6e2a7b2fac0b342a787ae24
Orso Meneghini <orso82@gmail.com>
2022-03-09 15:06:50 -0800

    Remove LazySets dependency

--- src/utils.jl
+++ src/utils.jl
@@ -75,3 +75,42 @@ function GASC(filename::String, case::Int)
     end
     return gasc
 end
+
+
+# ******************************************
+# Convex Hull
+# ******************************************
+struct Point
+    x::Float64
+    y::Float64
+end
+
+function Base.isless(p::Point, q::Point)
+    p.x < q.x || (p.x == q.x && p.y < q.y)
+end
+
+function isrightturn(p::Point, q::Point, r::Point)
+    (q.x - p.x) * (r.y - p.y) - (q.y - p.y) * (r.x - p.x) < 0
+end
+
+function grahamscan(points::Vector{Point})
+    sorted = sort(points)
+    upperhull = halfhull(sorted)
+    lowerhull = halfhull(reverse(sorted))
+    [upperhull..., lowerhull[2:end-1]...]
+end
+
+function convex_hull(xy::Vector)
+    return [(k.x,k.y) for k in grahamscan([Point(xx,yx) for (xx,yx) in xy])]
+end
+
+function halfhull(points::Vector{Point})
+    halfhull = points[1:2]
+    for p in points[3:end]
+        push!(halfhull, p)
+        while length(halfhull) > 2 && !isrightturn(halfhull[end-2:end]...)
+            deleteat!(halfhull, length(halfhull) - 1)
+        end
+    end
+    halfhull
+end
