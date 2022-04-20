c659e661b05d974f0ff9e54679617062155128ee
Orso Meneghini <orso82@gmail.com>
2022-04-20 00:33:44 -0700

    costing actor with HCD components

--- src/actors/costing_actor.jl
+++ src/actors/costing_actor.jl
@@ -7,7 +7,7 @@ function unit_cost(material::String)
     if material == "Vacuum"
         return 0.0 # $M/m^3
     elseif material == "ReBCO"
-        return 87.5/2 # $M/m^3
+        return 87.5 / 2 # $M/m^3
     elseif material == "Nb3Sn"
         return 1.66 # $M/m^3
     elseif contains(lowercase(material), "steel")
@@ -21,12 +21,28 @@ function unit_cost(material::String)
     elseif material == "lithium-lead"
         return 0.75 # $M/m^3
     elseif material == "FLiBe"
-        return 0.75*3 # $M/m^3
+        return 0.75 * 3 # $M/m^3
     elseif contains(lowercase(material), "plasma")
         return 0.0 # $M/m^3
     else
         error("Material `$material` has no price \$M/m³")
     end
+end
+
+function cost(ecl::IMAS.ec_launchers__launcher)
+    ecl.available_launch_power / 1E6 * 3.0 # $/W #ARIES
+end
+
+function cost(ica::IMAS.ic_antennas__antenna)
+    ica.available_launch_power / 1E6 * 1.64 #$/W ARIES
+end
+
+function cost(lha::IMAS.lh_antennas__antenna)
+    lha.available_launch_power / 1E6 * 2.13 #$/W ARIES
+end
+
+function cost(nbu::IMAS.nbi__unit)
+    nbu.available_launch_power / 1E6 * 4.93 #$/W ARIES
 end
 
 function unit_cost(coil_tech::Union{IMAS.build__tf__technology,IMAS.build__oh__technology,IMAS.build__pf_active__technology})
@@ -63,6 +79,33 @@ function CostingActor(dd::IMAS.dd, act::ActorParameters; kw...)
 end
 
 function step(actor::CostingActor)
+    dd = actor.dd
+    cst = dd.costing
+    empty!(cst)
+
+    # TOKAMAK
+    sys = resize!(cst.system, "name" => "tokamak")
+    for layer in dd.build.layer
+        if layer.fs == Int(_lfs_)
+            continue # avoid double counting of layers
+        end
+        c = cost(layer)
+        if c > 0
+            sub = resize!(sys.subsystem, "name" => replace(layer.name, r"^hfs " => ""))
+            sub.cost = c
+        end
+    end
+
+    # HCD
+    sys = resize!(cst.system, "name" => "HCD")
+    for hcd in vcat(dd.ec_launchers.launcher, dd.ic_antennas.antenna, dd.lh_antennas.antenna, dd.nbi.unit)
+        c = cost(hcd)
+        if c > 0
+            sub = resize!(sys.subsystem, "name" => hcd.name)
+            sub.cost = c
+        end
+    end
+
     return actor
 end
 
@@ -77,3 +120,9 @@ function cost(layer::IMAS.build__layer)
         unit_cost(layer.material) * layer.volume
     end
 end
+
+# unitCostShields = 0.29  # $M/m^3
+# unitCostBlanket = 0.75  # $M/m^3
+# unitCostStructure = 0.36  # $M/m^3
+# unitCostPowerAux = 5.3  # $M/MW
+# unitCostDivertor = 0.114  # $M/m^2
