3bf285440fc6734b89c5c15e90c394f5b1e3ff78
Orso Meneghini <orso82@gmail.com>
2022-04-22 22:03:24 -0700

    reorder ions so that He is last

--- src/ddinit/init_core_profiles.jl
+++ src/ddinit/init_core_profiles.jl
@@ -114,16 +114,16 @@ function init_core_profiles(
     cp1d.rotation_frequency_tor_sonic = rot_core .* (1.0 .- cp1d.grid.rho_tor_norm)
 
     # Set ions
-    # He == 1
-    # DT == 2
-    # Imp ==3
-    ion = resize!(cp1d.ion, "label" => "He")
-    fill!(ion, IMAS.ion_element(ion_symbol=:He))
+    # DT == 1
+    # Imp == 2
+    # He == 3
     ion = resize!(cp1d.ion, "label" => String(bulk))
     fill!(ion, IMAS.ion_element(ion_symbol=bulk))
     @assert ion.element[1].z_n == 1 "Bulk ion must be a Hydrogen isotope [:H, :D, :DT, :T]"
     ion = resize!(cp1d.ion, "label" => String(impurity))
     fill!(ion, IMAS.ion_element(ion_symbol=impurity))
+    ion = resize!(cp1d.ion, "label" => "He")
+    fill!(ion, IMAS.ion_element(ion_symbol=:He))
 
     # pedestal
     @ddtime summary.local.pedestal.n_e.value = ne_ped
@@ -144,32 +144,36 @@ function init_core_profiles(
     cp1d.electrons.density = Hmode_profiles(0.5 * ne_ped, ne_ped, ne_core, ngrid, n_shaping, n_shaping, w_ped)
     # Zeff and quasi neutrality for a helium constant fraction with one impurity specie
     niFraction = zeros(3)
-    # He == 1
-    # DT == 2
-    # Imp ==3
+    # DT == 1
+    # Imp == 2
+    # He == 3
     zimp = IMAS.ion_element(ion_symbol=impurity).element[1].z_n
-    niFraction[1] = helium_fraction
-    niFraction[2] = (zimp - zeff + 4 * niFraction[1] - 2 * zimp * niFraction[1]) / (zimp - 1)
-    niFraction[3] = (zeff - niFraction[2] - 4 * niFraction[1]) / zimp^2
-
+    niFraction[3] = helium_fraction
+    niFraction[1] = (zimp - zeff + 4 * niFraction[3] - 2 * zimp * niFraction[3]) / (zimp - 1)
+    niFraction[2] = (zeff - niFraction[1] - 4 * niFraction[3]) / zimp^2
     @assert !any(niFraction .< 0.0) "zeff impossible to match for given helium fraction [$helium_fraction] and zeff [$zeff]"
-
     for i = 1:length(cp1d.ion)
         cp1d.ion[i].density = cp1d.electrons.density .* niFraction[i]
     end
+
     # Set temperatures
     eqt = eq.time_slice[]
     betaN = eqt.global_quantities.beta_normal
     Bt = @ddtime eq.vacuum_toroidal_field.b0
     Ip = eqt.global_quantities.ip
     a = eqt.boundary.minor_radius
-
     Te_core = 10.0 * betaN * abs(Bt * (Ip / 1e6)) / a / (ne_core / 1e20) / (2.0 * 1.6e1 * 4.0 * pi * 1.0e-4)
     Te_ped = Te_core / 4
     cp1d.electrons.temperature = Hmode_profiles(80.0, Te_ped, Te_core, ngrid, T_shaping, T_shaping, w_ped)
     for i = 1:length(cp1d.ion)
         cp1d.ion[i].temperature = cp1d.electrons.temperature ./ T_ratio
     end
+
+    # remove He if not present
+    if sum(niFraction[3]) == 0.0
+        deleteat!(cp1d.ion,3)
+    end
+
     # ejima
     IMAS.set_time_array(cp.global_quantities, :ejima, ejima)
     return cp
