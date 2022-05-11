dbcbc7ff59a45801347b4d71a7bd9a52a662a481
Orso Meneghini <orso82@gmail.com>
2022-05-11 10:50:39 -0700

    Plotting of MultiobjectiveOptimizationResults

--- src/workflows/optimization_workflow.jl
+++ src/workflows/optimization_workflow.jl
@@ -2,12 +2,12 @@ import Distributed
 import OrderedCollections
 
 mutable struct MultiobjectiveOptimizationResults
-    workflow::Union{DataType, Function}
+    workflow::Union{DataType,Function}
     ini::ParametersInit
     act::ParametersActor
     state::Metaheuristics.State
-    opt_ini::Vector{Parameter}
-    objectives_functions::Vector{ObjectiveFunction}
+    opt_ini::Vector{<:Parameter}
+    objectives_functions::Vector{<:ObjectiveFunction}
 end
 
 """
@@ -25,7 +25,7 @@ Multi-objective optimization of either an `actor(dd, act)` or a `workflow(ini, a
 function workflow_multiobjective_optimization(
     ini::ParametersInit,
     act::ParametersActor,
-    actor_or_workflow::Union{DataType, Function},
+    actor_or_workflow::Union{DataType,Function},
     objectives_functions::Vector{<:ObjectiveFunction}=ObjectiveFunction[];
     N::Int=10,
     iterations::Int=N,
@@ -76,4 +76,86 @@ function workflow_multiobjective_optimization(
     @time state = Metaheuristics.optimize(X -> optimization_engine(ini, act, actor_or_workflow, X, opt_ini, objectives_functions, p), bounds, algorithm)
 
     return MultiobjectiveOptimizationResults(actor_or_workflow, ini, act, state, opt_ini, objectives_functions)
+end
+
+function pretty_label(objective_function::ObjectiveFunction, units="")
+    txt = split(string(objective_function.name), "_")[2]
+    if length(units) > 0
+        txt *= " [$units]"
+    elseif length(objective_function.units) > 0
+        txt *= " [$(objective_function.units)]"
+    end
+    return txt
+end
+
+function pretty_label(parameter::Parameter, units="")
+    txt = replace(string(parameter._name), "_" => " ")
+    if length(units) > 0
+        txt *= " [$units]"
+    elseif length(parameter.units) > 0
+        txt *= " [$(parameter.units)]"
+    end
+    return txt
+end
+
+@recipe function plot_MultiobjectiveOptimizationResults(results::MultiobjectiveOptimizationResults, indexes=[1, 2, 3]; design_space::Bool=false, pareto=true)
+
+    if design_space
+        arg = :x
+        labels = results.opt_ini
+    else
+        arg = :f
+        labels = results.objectives_functions
+    end
+
+    x = []
+    y = []
+    z = []
+    c = []
+
+    @series begin
+        seriestype --> :scatter
+        label --> ""
+        for (generation, res) in enumerate(results.state.convergence)
+            if pareto
+                sol = Metaheuristics.get_non_dominated_solutions(res.population)
+            else
+                sol = res.population
+            end
+
+            if length(indexes) == 1 || length(sol[1].x) == 1
+                xlabel --> "Run number"
+                ylabel --> pretty_label(labels[indexes[1]])
+                append!(x, (getfield(s,arg)[indexes[1]] for s in sol))
+            elseif length(indexes) == 2 || length(sol[1].x) == 2
+                append!(x, (getfield(s,arg)[indexes[1]] for s in sol))
+                append!(y, (getfield(s,arg)[indexes[2]] for s in sol))
+                xlabel --> pretty_label(labels[indexes[1]])
+                ylabel --> pretty_label(labels[indexes[2]])
+            elseif length(indexes) == 3 || length(sol[1].x) == 3
+                append!(x, (getfield(s,arg)[indexes[1]] for s in sol))
+                append!(y, (getfield(s,arg)[indexes[2]] for s in sol))
+                append!(z, (getfield(s,arg)[indexes[3]] for s in sol))
+                xlabel --> pretty_label(labels[indexes[1]])
+                ylabel --> pretty_label(labels[indexes[2]])
+                zlabel --> pretty_label(labels[indexes[3]])
+            end
+
+            #append!(c, (s.x[3] for s in sol))
+            append!(c, (generation for s in sol))
+        end
+        
+        if length(c) > 0
+            marker_z --> c
+        end
+
+        if isempty(y)
+            x
+        elseif isempty(z)
+            x, y
+        else
+            x, y, z
+        end
+    end
+
 end
