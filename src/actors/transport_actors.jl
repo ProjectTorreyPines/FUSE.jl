da9f9e48b1f4c722dd36a3049fa07bafd08d09b8
Orso Meneghini <orso82@gmail.com>
2022-05-19 10:38:08 -0700

    support confinement factor and ds03 scaling

--- src/actors/transport_actors.jl
+++ src/actors/transport_actors.jl
@@ -12,13 +12,14 @@ end
 
 function ParametersActor(::Type{Val{:ActorTauenn}})
     par = ParametersActor(nothing)
-    par.error = Entry(Real, "", "Error level"; default=1E-2)
+    par.error = Entry(Real, "", "Target convergence error"; default=1E-2)
     par.eped_factor = Entry(Real, "", "Scaling parameter for EPED-NN prediction"; default=1.0)
-    par.rho_fluxmatch = Entry(Real, "", "radial location where flux-macthing is done"; default=0.6)
-    par.T_shaping = Entry(Real, "", "shaping coefficient for the temperature profile"; default=1.8)
-    par.temp_pedestal_ratio = Entry(Real, "", "ion to electron temperature ratio in the pedestal"; default=1.0)
-    par.transport_model = Switch([:tglfnn, :h98y2, :tglf], "", "transport model"; default=:tglfnn)
-    par.warn_nn_train_bounds = Entry(Bool, "", "warn if EPED-NN / TGLF-NN training bounds are exceeded"; default=false)
+    par.rho_fluxmatch = Entry(Real, "", "Radial location where flux-macthing is done"; default=0.6)
+    par.T_shaping = Entry(Real, "", "Shaping coefficient for the temperature profile"; default=1.8)
+    par.temp_pedestal_ratio = Entry(Real, "", "Ion to electron temperature ratio in the pedestal"; default=1.0)
+    par.transport_model = Switch([:tglfnn, :tglf, :h98y2, :ds03], "", "Transport model"; default=:tglfnn)
+    par.warn_nn_train_bounds = Entry(Bool, "", "Warn if EPED-NN / TGLF-NN training bounds are exceeded"; default=false)
+    par.confinement_factor = Entry(Real, "", "Confinement multiplier"; default=1.0)
     par.do_plot = Entry(Bool, "", "plot"; default=false)
     par.verbose = Entry(Bool, "", "verbose"; default=false)
     return par
@@ -37,6 +38,7 @@ function ActorTauenn(dd::IMAS.dd, act::ParametersActor; kw...)
         T_shaping=par.T_shaping,
         temp_pedestal_ratio=par.temp_pedestal_ratio,
         transport_model=par.transport_model,
+        confinement_factor=par.confinement_factor,
         warn_nn_train_bounds=par.warn_nn_train_bounds)
     step(actor; verbose=par.verbose)
     finalize(actor)
