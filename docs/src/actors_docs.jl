d664b6fe559cf281fb1c101139c0ff08f81fd685
Orso Meneghini <orso82@gmail.com>
2022-07-20 00:13:38 -0700

    documentation for Fidelity hierarchy in actors

--- docs/src/actors_docs.jl
+++ docs/src/actors_docs.jl
@@ -2,9 +2,24 @@ txt = ["""
 # Physics and Engineering Actors
 
 Physics and engineering **actors** are the fundamental building blocks of FUSE simulations:
-  * Actors operate exclusively on `IMAS.dd` data
-  * Actors functionality is controlled via `act` parameters
-  * Actors can be combined into other actors
+* Actors operate exclusively on `IMAS.dd` data
+* Actors functionality is controlled via `act` parameters
+* Actors can be combined into other actors
+
+Fidelity hierarchy is enabled by concept of *abstract* Vs *concrete* actors
+* Abstract actors define physics/component 
+* Concrete actors implement a specific model for that physics/component
+* For example:
+  ```
+  EquilibriumActor  <--  abstract
+  ├─ SolovevActor   <--  concrete
+  └─ CHEASEActor    <--  concrete
+  ```
+* `act.[AbstractActor].model` selects concrete actor being used
+* All concrete actors will expect data and fill the same enties in `dd`
+  * IMAS.jl expressions are key to make this work seamlessly
+* Where possible workflows should make use of abstract actors and not hardcode use of concrete actors
+
 ```@contents
     Pages = ["actors.md"]
     Depth = 3
