0c452d83cdfa2f4a179a2422633b1d1ed9f8833f
Orso Meneghini <orso82@gmail.com>
2022-09-08 09:57:58 -0700

    remove unnecessary FUSE.

--- src/remote.jl
+++ src/remote.jl
@@ -6,7 +6,7 @@ const url_worker = "tcp://127.0.0.1:8888"
 const url_client = "tcp://127.0.0.1:9999"
 
 function client_test(url::String="127.0.0.1"; url_client::String=url_client)
-    ini, act = FUSE.case_parameters(:FPP; version=:v1_demount, init_from=:scalars);
+    ini, act = case_parameters(:FPP; version=:v1_demount, init_from=:scalars);
     return remote_run(ini, act, url; url_client)
 end
 
@@ -18,10 +18,10 @@ end
 
 function remote_run(ini::ParametersAllInits, act::ParametersAllActors, url::String="127.0.0.1"; url_client::String=url_client)
     url_client = replace(url_client, "127.0.0.1"=>url)
-    payload = Dict("ini" => FUSE.par2dict(ini), "act" => FUSE.par2dict(act))
-    json_payload = FUSE.JSON.sprint(payload)
+    payload = Dict("ini" => par2dict(ini), "act" => par2dict(act))
+    json_payload = JSON.sprint(payload)
     tmp = Broker.client(json_payload, url_client)
-    dct = FUSE.JSON.parse(String(tmp))
+    dct = JSON.parse(String(tmp))
     return IMAS.dict2imas(dct, IMAS.dd())
 end
 
@@ -31,18 +31,18 @@ function worker_function(json_payload::String)
 
         # read json input
         data = JSON.parse(json_payload, dicttype=DataStructures.OrderedDict)
-        ini = FUSE.dict2par!(data["ini"], FUSE.ParametersAllInits())
-        act = FUSE.dict2par!(data["act"], FUSE.ParametersAllActors())
+        ini = dict2par!(data["ini"], ParametersAllInits())
+        act = dict2par!(data["act"], ParametersAllActors())
 
         # init
-        dd = FUSE.init(ini, act)
+        dd = init(ini, act)
 
         # run workflow
         act.ActorPFcoilsOpt.optimization_scheme = :none
-        FUSE.ActorWholeFacility(dd, act)
+        ActorWholeFacility(dd, act)
 
         # return dd data
-        return FUSE.JSON.sprint(IMAS.imas2dict(dd; freeze=false))
+        return JSON.sprint(IMAS.imas2dict(dd; freeze=false))
     catch e
         display(e)
         return string(e)
