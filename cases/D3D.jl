b2f8ae6b4850b8c70b730a51030b327036f5fbdf
Orso Meneghini <orso82@gmail.com>
2022-09-26 13:22:38 -0700

    small fixes

--- cases/D3D.jl
+++ cases/D3D.jl
@@ -40,7 +40,7 @@ function case_parameters(::Type{Val{:D3D}})::Tuple{ParametersAllInits,Parameters
 
     ini.nbi.power_launched = 5E6
     ini.nbi.beam_energy = 80e3
-    ini.nbi.beam_mass = 2
+    ini.nbi.beam_mass = 2.0
     ini.nbi.toroidal_angle = 20.0 / 180 * pi
 
     ini.target.flattop_duration = 5.0
