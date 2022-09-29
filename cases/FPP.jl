2c226b9597a3fe22e615d9690a11c2afcf3d1d79
Orso Meneghini <orso82@gmail.com>
2022-09-28 23:44:53 -0700

    add highbetap fpp STEP sample case

--- cases/FPP.jl
+++ cases/FPP.jl
@@ -27,7 +27,7 @@ function case_parameters(::Type{Val{:FPP}}; version::Symbol, init_from::Symbol):
     end
 
     if init_from == :ods
-        ini.ods.filename = joinpath(@__DIR__, "..", "sample", "FPPv1.0_demount_eq.json")
+        ini.ods.filename = joinpath(@__DIR__, "..", "sample", "highbetap_fpp_325_ods.json")
         act.ActorCXbuild.rebuild_wall = true # false to use wall from ODS
         act.ActorHFSsizing.fixed_aspect_ratio = true
     end
