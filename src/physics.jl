361b86f5eec6cb03df3e5fcf1a904fb4ed825434
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-01-05 09:27:45 -0800

    add check_ped_finder function

--- src/physics.jl
+++ src/physics.jl
@@ -850,3 +850,14 @@ function private_flux_regions_from_lcfs(mr::AbstractArray{T}, mz::AbstractArray{
 
     return rr_u, zz_u, rr_l, zz_l
 end
+
+"""
+    check_ped_finder(profile::AbstractVector{<:Real},psi_norm::AbstractVector{<:Real})
+
+Plots the profile and fitted profile for pedestal finding (region outside pedestal not important for fit)
+"""
+function check_ped_finder(profile::AbstractVector{<:Real},psi_norm::AbstractVector{<:Real})
+    ped_height,ped_width = IMAS.pedestal_finder(profile,psi_norm)
+    plot(psi_norm,profile,label="original profile")
+    plot!(psi_norm,IMAS.Hmode_profiles(profile[end], ped_height, profile[1], length(profile), 2.0, 2.0, ped_width),label="fitted profile (pedestal region is important only)")
+end
