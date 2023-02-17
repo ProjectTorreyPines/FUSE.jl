19400fa8c9e45b5d22c0ebf41202a1f7c6aec848
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-02-16 21:30:42 -0800

    adding ConstraintFunctionsLibrary

--- src/optimization.jl
+++ src/optimization.jl
@@ -51,12 +51,52 @@ ObjectiveFunction(:max_power_electric_net, "MW", dd -> @ddtime(dd.balance_of_pla
 ObjectiveFunction(:max_flattop, "hours", dd -> dd.build.oh.flattop_duration / 3600.0, Inf)
 ObjectiveFunction(:max_log10_flattop, "log₁₀(hours)", dd -> log10(dd.build.oh.flattop_duration / 3600.0), Inf)
 
+mutable struct ConstraintFunction
+    name::Symbol
+    units::String
+    func::Function
+    operation::Function
+    limit::Real
+    tolerance::Real
+    # inner constructor to register Constraint functions in ConstraintsFunctionsLibrary
+    ConstraintFunction(name, units, func,operation, limit ,tolerance) = begin
+        constraint = new(name, units, func,operation,limit,tolerance)
+        ConstraintFunctionsLibrary[constraint.name] = constraint
+        return constraint
+    end
+end
+
+function (constraint::ConstraintFunction)(dd::IMAS.dd)
+    if constraint.operation == ==
+        return sqrt((constraint.func(dd) - constraint.limit)^2) / constraint.limit < constraint.tolerance
+    elseif constraint.operation == >
+        return constraint.limit < (1+constraint.tolerance) * constraint.func(dd) 
+    elseif constraint.operation == <
+        return  (1+constraint.tolerance) * constraint.limit >  constraint.func(dd)
+    else
+        error("operation unknown $(constraint.operation)")
+    end
+end
+
+
+const ConstraintFunctionsLibrary = Dict{Symbol,ConstraintFunction}()
+constraint_value -> ConstraintFunction(:Beta_n_limit,"", dd -> dd.equilibrium.time_slice[].global_quantities.beta_normal,<,constraint_value,1e-3)
+constraint_value -> ConstraintFunction(:max_power_electric_net, "MW", dd -> @ddtime(dd.balance_of_plant.power_electric_net) / 1E6,==,constraint_value,1e-2)
+
 function Base.show(io::IO, f::ObjectiveFunction)
     printstyled(io, f.name; bold=true, color=:blue)
     print(io, " [$(f.units)]")
 end
 
-function optimization_engine(ini::ParametersAllInits, act::ParametersAllActors, actor_or_workflow::Union{DataType,Function}, x::AbstractVector, opt_ini::Vector{<:AbstractParameter}, objectives_functions::AbstractVector{<:ObjectiveFunction})
+function optimization_engine(
+    ini::ParametersAllInits,
+    act::ParametersAllActors,
+    actor_or_workflow::Union{DataType,Function},
+    x::AbstractVector,
+    opt_ini::Vector{<:AbstractParameter},
+    objectives_functions::AbstractVector{<:ObjectiveFunction},
+    constraint_functions::AbstractVector{<:ConstraintFunction}
+    )
     # update ini based on input optimization vector `x`
     for (optpar, xx) in zip(opt_ini, x)
         if typeof(optpar.value) <: Integer
@@ -74,20 +114,30 @@ function optimization_engine(ini::ParametersAllInits, act::ParametersAllActors, 
             dd = actor_or_workflow(ini, act)
         end
         # evaluate multiple objectives
-        return collect(map(f -> f(dd), objectives_functions)), x * 0, x * 0
+        return collect(map(f -> f(dd), objectives_functions)),collect(map(f -> f(dd), constraint_functions)), x * 0, x * 0
     catch
         # rethrow() 
         return [Inf for f in objectives_functions], x * 0, x * 0
     end
 end
 
-function optimization_engine(ini::ParametersAllInits, act::ParametersAllActors, actor_or_workflow::Union{DataType,Function}, X::AbstractMatrix, opt_ini, objectives_functions::AbstractVector{<:ObjectiveFunction}, p)
+function optimization_engine(
+    ini::ParametersAllInits,
+    act::ParametersAllActors,
+    actor_or_workflow::Union{DataType,Function},
+    X::AbstractMatrix,
+    Y::AbstractMatrix,
+    opt_ini,
+    objectives_functions::AbstractVector{<:ObjectiveFunction},
+    constraints_functions::AbstractVector{<:ConstraintFunction},
+    p)
+
     # parallel evaluation of a generation
     ProgressMeter.next!(p)
-    tmp = Distributed.pmap(x -> optimization_engine(ini, act, actor_or_workflow, x, opt_ini, objectives_functions), [X[k, :] for k in 1:size(X)[1]])
+    tmp = Distributed.pmap(x -> optimization_engine(ini, act, actor_or_workflow, x, opt_ini, objectives_functions, constraints_functions), [X[k, :] for k in 1:size(X)[1]], [Y[k, :] for k in 1:size(Y)[1]])
     F = zeros(size(X)[1], length(objectives_functions))
     G = similar(X)
-    H = similar(X)
+    H = zeros(size(Y)[1], length(constraints_functions))
     for k in 1:size(X)[1]
         F[k, :] .= tmp[k][1]
         G[k, :] .= tmp[k][2]
