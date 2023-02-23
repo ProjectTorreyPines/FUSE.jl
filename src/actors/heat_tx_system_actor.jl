65071cb76df7657c8dd081caca5095a0d410b3e8
Orso Meneghini <orso82@gmail.com>
2023-02-23 01:07:25 -0800

    Formatting

--- src/actors/heat_tx_system_actor.jl
+++ src/actors/heat_tx_system_actor.jl
@@ -3,7 +3,7 @@
 #= ================= =#
 # ACTOR FOR THE INTERMEDIATE HEAT TRANSFER SYSTEM
 
-mutable struct ActorHeatTxSystem<: FacilityAbstractActor
+mutable struct ActorHeatTxSystem <: FacilityAbstractActor
     dd::IMAS.dd
     par::ParametersActor
 end
@@ -14,26 +14,26 @@ Base.@kwdef mutable struct FUSEparameters__ActorHeatTxSystem{T} <: ParametersAct
     _parent::WeakRef = WeakRef(Nothing)
     _name::Symbol = :not_set
     #  BREEDER INFO
-    breeder_pressure::Entry{T} = Entry(T, "Pa", "Pressure across pump in breeder fluid circuit"; default = 1e6)
-    breeder_ΔP::Entry{T} = Entry(T,"Pa", "Pressure drop during cooling and heat exchanger"; default = 0.25*10^6)
-    breeder_low_temp::Entry{T} = Entry(T, "K", "Minimum breeder fluid temperature after primary heat exchange"; default=700+273.15)
-    breeder_hi_temp::Entry{T} = Entry(T, "K", "Maximum breeder fluid temperature at Blanket Outlet"; default=1100+273.15)
+    breeder_pressure::Entry{T} = Entry(T, "Pa", "Pressure across pump in breeder fluid circuit"; default=1e6)
+    breeder_ΔP::Entry{T} = Entry(T, "Pa", "Pressure drop during cooling and heat exchanger"; default=0.25 * 10^6)
+    breeder_low_temp::Entry{T} = Entry(T, "K", "Minimum breeder fluid temperature after primary heat exchange"; default=700 + 273.15)
+    breeder_hi_temp::Entry{T} = Entry(T, "K", "Maximum breeder fluid temperature at Blanket Outlet"; default=1100 + 273.15)
     breeder_η_pump::Entry{T} = Entry(T, "-", "Breeder pump effeciency"; default=0.7)
     #  BLANKET COOLING INFO
-    coolant_pressure::Entry{T} = Entry(T, "Pa", "Pressure across pump in coolant fluid ciruit"; default = 10e6)
-    coolant_ΔP::Entry{T} = Entry(T,"Pa", "Pressure drop during cooling and heat exchanger"; default = 0.3*10^6)    
+    coolant_pressure::Entry{T} = Entry(T, "Pa", "Pressure across pump in coolant fluid ciruit"; default=10e6)
+    coolant_ΔP::Entry{T} = Entry(T, "Pa", "Pressure drop during cooling and heat exchanger"; default=0.3 * 10^6)
     divertor_η_pump::Entry{T} = Entry(T, "-", "Divertor pump effeciency"; default=0.89)
-    divertor_max_temp::Entry{T} = Entry(T, "K", "Maximum Coolant Outlet Temperature"; default=650+273.15)
+    divertor_max_temp::Entry{T} = Entry(T, "K", "Maximum Coolant Outlet Temperature"; default=650 + 273.15)
     blanket_η_pump::Entry{T} = Entry(T, "-", "Blanket pump effeciency"; default=0.89)
-    blanket_max_temp::Entry{T} = Entry(T, "K",  "Maximum Coolant Outlet Temperature"; default=450+273.15)
+    blanket_max_temp::Entry{T} = Entry(T, "K", "Maximum Coolant Outlet Temperature"; default=450 + 273.15)
     # ASSUMED 
-    radiation_factor::Entry{T} = Entry(T, "-", "Assumed factor of multiplication for the power absorbed by the blanket";default=2.5)
-    breeder_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the breeder - cycle heat exchanger";default=0.9)
-    divertor_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the divertor - cycle heat exchanger";default=0.9)
-    blanket_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the blanket - cycle heat exchanger";default=0.9)
-    breeder_fluid::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-",  "Breeder coolant fluid"; default = :Pbli)
-    blanket_coolant::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-",  "Breeder coolant fluid"; default = :He)
-    divertor_coolant::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-",  "Breeder coolant fluid"; default = :He)
+    radiation_factor::Entry{T} = Entry(T, "-", "Assumed factor of multiplication for the power absorbed by the blanket"; default=2.5)
+    breeder_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the breeder - cycle heat exchanger"; default=0.9)
+    divertor_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the divertor - cycle heat exchanger"; default=0.9)
+    blanket_HX_ϵ::Entry{T} = Entry(T, "-", "Effectiveness of the blanket - cycle heat exchanger"; default=0.9)
+    breeder_fluid::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-", "Breeder coolant fluid"; default=:Pbli)
+    blanket_coolant::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-", "Breeder coolant fluid"; default=:He)
+    divertor_coolant::Switch{Symbol} = Switch(Symbol, coolant_fluid, "-", "Breeder coolant fluid"; default=:He)
 end
 
 """
@@ -63,9 +63,9 @@ function _step(actor::ActorHeatTxSystem)
     par = actor.par
     bop = dd.balance_of_plant
 
-    bop.heat_tx_system.divertor.working_fluid  = string(par.divertor_coolant)
-    bop.heat_tx_system.blanket.working_fluid   = string(par.blanket_coolant)
-    bop.heat_tx_system.breeder.working_fluid   = string(par.breeder_fluid)
+    bop.heat_tx_system.divertor.working_fluid = string(par.divertor_coolant)
+    bop.heat_tx_system.blanket.working_fluid = string(par.blanket_coolant)
+    bop.heat_tx_system.breeder.working_fluid = string(par.breeder_fluid)
 
     bop.time = dd.core_profiles.time
 
@@ -73,99 +73,99 @@ function _step(actor::ActorHeatTxSystem)
     # THERMAL #
     # ======= #
     bop_IHTS = bop.heat_tx_system
-    
+
     # breeder_heat_load   = (sum([bmod.time_slice[time].power_thermal_extracted for bmod in dd.blanket.module]) for time in bop.time)
-    breeder_heat_load   = abs.(sum([bmod.time_slice[].power_thermal_extracted for bmod in dd.blanket.module]))
-    divertor_heat_load  = abs.(sum([(@ddtime(div.power_incident.data)) for div in dd.divertors.divertor]))
-    blanket_heat_load   = abs.(IMAS.radiation_losses(dd.core_sources)*par.radiation_factor);
-    
+    breeder_heat_load = abs.(sum([bmod.time_slice[].power_thermal_extracted for bmod in dd.blanket.module]))
+    divertor_heat_load = abs.(sum([(@ddtime(div.power_incident.data)) for div in dd.divertors.divertor]))
+    blanket_heat_load = abs.(IMAS.radiation_losses(dd.core_sources) * par.radiation_factor)
+
     # @show divertor_heat_load
     # @show blanket_heat_load
     # @show breeder_heat_load
 
-    cp_low,rho_low  = pbLi_props(par.breeder_low_temp);
-    cp_hi,rho_hi    = pbLi_props(par.breeder_hi_temp);
-    cp_ave = (cp_hi+cp_low)/2;
-    rho_ave = (rho_hi+rho_low)/2;
-    v_ave = 1/rho_ave;
+    cp_low, rho_low = pbLi_props(par.breeder_low_temp)
+    cp_hi, rho_hi = pbLi_props(par.breeder_hi_temp)
+    cp_ave = (cp_hi + cp_low) / 2
+    rho_ave = (rho_hi + rho_low) / 2
+    v_ave = 1 / rho_ave
 
-    cp_he = 5.1926e3;
+    cp_he = 5.1926e3
 
-    cv_cycle    = 3.1156e3;    
-    k_cycle = cp_he/cv_cycle;
-    kcoeff      = (k_cycle-1)/k_cycle;
+    cv_cycle = 3.1156e3
+    k_cycle = cp_he / cv_cycle
+    kcoeff = (k_cycle - 1) / k_cycle
 
     # inital values as guess
-    ΔT_cycle_breeder = par.breeder_hi_temp - par.divertor_max_temp;
+    ΔT_cycle_breeder = par.breeder_hi_temp - par.divertor_max_temp
 
-    @ddtime(bop.thermal_cycle.flow_rate = breeder_heat_load/(ΔT_cycle_breeder*cp_he))
-    
-    cycle_flow = @ddtime(bop.thermal_cycle.flow_rate) ;
+    @ddtime(bop.thermal_cycle.flow_rate = breeder_heat_load / (ΔT_cycle_breeder * cp_he))
 
-    @ddtime(bop_IHTS.blanket.flow_rate = cycle_flow);
-    @ddtime(bop_IHTS.divertor.flow_rate = cycle_flow);
+    cycle_flow = @ddtime(bop.thermal_cycle.flow_rate)
 
-        # INTIALIZING DD WITH HEAT LAODS
-    @ddtime(bop_IHTS.divertor.heat_load    = divertor_heat_load)
-    @ddtime(bop_IHTS.blanket.heat_load     = blanket_heat_load)
-    @ddtime(bop_IHTS.breeder.heat_load     = breeder_heat_load)
+    @ddtime(bop_IHTS.blanket.flow_rate = cycle_flow)
+    @ddtime(bop_IHTS.divertor.flow_rate = cycle_flow)
+
+    # INTIALIZING DD WITH HEAT LAODS
+    @ddtime(bop_IHTS.divertor.heat_load = divertor_heat_load)
+    @ddtime(bop_IHTS.blanket.heat_load = blanket_heat_load)
+    @ddtime(bop_IHTS.breeder.heat_load = breeder_heat_load)
 
     #basic params
-    ΔT_blanket  = blanket_heat_load/(cycle_flow*cp_he)
-    ΔT_divertor = divertor_heat_load/(cycle_flow*cp_he)
-    
-    rp_div = par.coolant_pressure/(par.coolant_pressure-par.coolant_ΔP);
-    rp_blk = rp_div;
+    ΔT_blanket = blanket_heat_load / (cycle_flow * cp_he)
+    ΔT_divertor = divertor_heat_load / (cycle_flow * cp_he)
+
+    rp_div = par.coolant_pressure / (par.coolant_pressure - par.coolant_ΔP)
+    rp_blk = rp_div
 
     @ddtime(bop_IHTS.blanket.outlet_temperature = par.blanket_max_temp)
     @ddtime(bop_IHTS.divertor.outlet_temperature = par.divertor_max_temp)
 
-    blk_after_comp =  par.blanket_max_temp-ΔT_blanket
-    div_after_comp = par.divertor_max_temp-ΔT_divertor
-    
-    @ddtime(bop_IHTS.blanket.inlet_temperature  = blk_after_comp)
+    blk_after_comp = par.blanket_max_temp - ΔT_blanket
+    div_after_comp = par.divertor_max_temp - ΔT_divertor
+
+    @ddtime(bop_IHTS.blanket.inlet_temperature = blk_after_comp)
     @ddtime(bop_IHTS.divertor.inlet_temperature = div_after_comp)
 
-    blk_pre_comp = blk_after_comp/(1+(rp_blk^kcoeff-1)/par.blanket_η_pump);
-    div_pre_comp = div_after_comp/(1+(rp_div^kcoeff-1)/par.divertor_η_pump);
+    blk_pre_comp = blk_after_comp / (1 + (rp_blk^kcoeff - 1) / par.blanket_η_pump)
+    div_pre_comp = div_after_comp / (1 + (rp_div^kcoeff - 1) / par.divertor_η_pump)
 
-    @ddtime(bop_IHTS.blanket.HX_outlet_temperature  =   blk_pre_comp)
+    @ddtime(bop_IHTS.blanket.HX_outlet_temperature = blk_pre_comp)
     @ddtime(bop_IHTS.divertor.HX_outlet_temperature = div_pre_comp)
 
-    @ddtime(bop_IHTS.blanket.circulator_power  =    gasCirculator(rp_blk,blk_pre_comp,par.blanket_η_pump,cycle_flow))
-    @ddtime(bop_IHTS.divertor.circulator_power  =   gasCirculator(rp_div,div_pre_comp,par.divertor_η_pump,cycle_flow))
+    @ddtime(bop_IHTS.blanket.circulator_power = gasCirculator(rp_blk, blk_pre_comp, par.blanket_η_pump, cycle_flow))
+    @ddtime(bop_IHTS.divertor.circulator_power = gasCirculator(rp_div, div_pre_comp, par.divertor_η_pump, cycle_flow))
 
-    w_isentropic    = v_ave*par.breeder_ΔP
-    w_actual        = w_isentropic/par.breeder_η_pump
+    w_isentropic = v_ave * par.breeder_ΔP
+    w_actual = w_isentropic / par.breeder_η_pump
 
-    Tin_breeder =   par.breeder_low_temp+(w_actual-w_isentropic)/cp_ave;   #temperature after pump
-    ΔT_breeder  =   par.breeder_hi_temp-Tin_breeder;
-    breeder_mflow =  breeder_heat_load/(cp_ave*ΔT_breeder);
+    Tin_breeder = par.breeder_low_temp + (w_actual - w_isentropic) / cp_ave   #temperature after pump
+    ΔT_breeder = par.breeder_hi_temp - Tin_breeder
+    breeder_mflow = breeder_heat_load / (cp_ave * ΔT_breeder)
 
-        # FULLY DEFINING BREEDER
-    @ddtime(bop_IHTS.breeder.HX_outlet_temperature  = par.breeder_low_temp);
-    @ddtime(bop_IHTS.breeder.inlet_temperature  = Tin_breeder);
+    # FULLY DEFINING BREEDER
+    @ddtime(bop_IHTS.breeder.HX_outlet_temperature = par.breeder_low_temp)
+    @ddtime(bop_IHTS.breeder.inlet_temperature = Tin_breeder)
     @ddtime(bop_IHTS.breeder.outlet_temperature = par.breeder_hi_temp)
-    @ddtime(bop_IHTS.breeder.flow_rate          = breeder_mflow);
-    @ddtime(bop_IHTS.breeder.circulator_power   = breeder_mflow*v_ave*par.breeder_ΔP/par.breeder_η_pump);
+    @ddtime(bop_IHTS.breeder.flow_rate = breeder_mflow)
+    @ddtime(bop_IHTS.breeder.circulator_power = breeder_mflow * v_ave * par.breeder_ΔP / par.breeder_η_pump)
 
     return actor
 end
 
 function pbLi_props(Temperature)
-#temperature input in celcius
-specific_heat   = (0.195 - 9.116*10^(-6).*(Temperature)).*1000   #J/kgK
-density         =  10520.35-1.19051.*(Temperature); 
-return [specific_heat,density]
+    #temperature input in celcius
+    specific_heat = (0.195 - 9.116 * 10^(-6) .* (Temperature)) .* 1000   #J/kgK
+    density = 10520.35 - 1.19051 .* (Temperature)
+    return [specific_heat, density]
 end
 
-function gasCirculator(rp,Tin,effC,mflow)
-    nstages = 1;
+function gasCirculator(rp, Tin, effC, mflow)
+    nstages = 1
 
-    cp = 5.1926e3;
-    cv = 3.1156e3;
-    a1c = (effC*(rp^(1/nstages))^(cv/cp) - (rp^(1/nstages))^(cv/cp) + rp^(1/nstages))/(effC*(rp^(1/nstages))^(cv/cp));
+    cp = 5.1926e3
+    cv = 3.1156e3
+    a1c = (effC * (rp^(1 / nstages))^(cv / cp) - (rp^(1 / nstages))^(cv / cp) + rp^(1 / nstages)) / (effC * (rp^(1 / nstages))^(cv / cp))
 
-    work_in   = cp*(a1c-1)*Tin*mflow;
+    work_in = cp * (a1c - 1) * Tin * mflow
     return work_in
 end
