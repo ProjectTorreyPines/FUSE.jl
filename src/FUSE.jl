120029a407ed5d43be5bdee29af3498fe6408528
Orso Meneghini <orso82@gmail.com>
2023-03-02 10:26:31 -0800

    split compound actors and move them to subfolder

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -68,8 +68,10 @@ include(joinpath("actors", "tglf_actor.jl"))
 include(joinpath("actors", "core_transport_actor.jl"))
 include(joinpath("actors", "transport_solver_actor.jl"))
 include(joinpath("actors", "limits_actor.jl"))
+
 # NOTE: compound actors should be defined last
-include(joinpath("actors", "compound_actors.jl"))
+include(joinpath("actors", "compound", "equilibrium_transport_actor.jl"))
+include(joinpath("actors", "compound", "whole_facility_actor.jl"))
 
 #= ========== =#
 #  PARAMETERS  #
---
+++ src/actors/compound/whole_facility_actor.jl
@@ -0,0 +1,94 @@
+#= ================== =#
+#  ActorWholeFacility  #
+#= ================== =#
+Base.@kwdef mutable struct FUSEparameters__ActorWholeFacility{T} <: ParametersActor where {T<:Real}
+    _parent::WeakRef = WeakRef(nothing)
+    _name::Symbol = :not_set
+end
+
+mutable struct ActorWholeFacility <: FacilityAbstractActor
+    dd::IMAS.dd
+    par::FUSEparameters__ActorWholeFacility
+    act::ParametersAllActors
+    EquilibriumTransport::Union{Nothing,ActorEquilibriumTransport}
+    PlasmaLimits::Union{Nothing,ActorPlasmaLimits}
+    HFSsizing::Union{Nothing,ActorHFSsizing}
+    LFSsizing::Union{Nothing,ActorLFSsizing}
+    CXbuild::Union{Nothing,ActorCXbuild}
+    PFcoilsOpt::Union{Nothing,ActorPFcoilsOpt}
+    PassiveStructures::Union{Nothing,ActorPassiveStructures}
+    Neutronics::Union{Nothing,ActorNeutronics}
+    Blanket::Union{Nothing,ActorBlanket}
+    Divertors::Union{Nothing,ActorDivertors}
+    BalanceOfPlant::Union{Nothing,ActorBalanceOfPlant}
+    Costing::Union{Nothing,ActorCosting}
+end
+
+"""
+    ActorWholeFacility(dd::IMAS.dd, act::ParametersAllActors; kw...)
+
+Compound actor that runs all the physics, engineering and costing actors needed to model the whole plant:
+* ActorEquilibriumTransport
+* ActorPlasmaLimits
+* ActorHFSsizing
+* ActorLFSsizing
+* ActorCXbuild
+* ActorPFcoilsOpt
+* ActorPassiveStructures
+* ActorNeutronics
+* ActorBlanket
+* ActorDivertors
+* ActorBalanceOfPlant
+* ActorCosting
+
+!!! note 
+    Stores data in `dd`
+"""
+function ActorWholeFacility(dd::IMAS.dd, act::ParametersAllActors; kw...)
+    par = act.ActorWholeFacility(kw...)
+    actor = ActorWholeFacility(dd, par, act)
+    step(actor)
+    finalize(actor)
+    return actor
+end
+
+function ActorWholeFacility(dd::IMAS.dd, par::FUSEparameters__ActorWholeFacility, act::ParametersAllActors; kw...)
+    logging_actor_init(ActorWholeFacility)
+    par = par(kw...)
+
+    ActorWholeFacility(dd, par, act,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing,
+        nothing)
+end
+
+function _step(actor::ActorWholeFacility)
+    dd = actor.dd
+    act = actor.act
+    actor.EquilibriumTransport = ActorEquilibriumTransport(dd, act)
+    actor.PlasmaLimits == ActorPlasmaLimits(dd, act)
+    actor.HFSsizing = ActorHFSsizing(dd, act)
+    actor.LFSsizing = ActorLFSsizing(dd, act)
+    actor.CXbuild = ActorCXbuild(dd, act)
+    actor.PFcoilsOpt = ActorPFcoilsOpt(dd, act)
+    if act.ActorPFcoilsOpt.update_equilibrium && act.ActorCXbuild.rebuild_wall
+        actor.CXbuild = ActorCXbuild(dd, act)
+        actor.PFcoilsOpt = ActorPFcoilsOpt(dd, act; update_equilibrium=false)
+    end
+    actor.PassiveStructures = ActorPassiveStructures(dd, act)
+    actor.Neutronics = ActorNeutronics(dd, act)
+    actor.Blanket = ActorBlanket(dd, act)
+    actor.Divertors = ActorDivertors(dd, act)
+    actor.BalanceOfPlant = ActorBalanceOfPlant(dd, act)
+    actor.Costing = ActorCosting(dd, act)
+    return actor
+end
