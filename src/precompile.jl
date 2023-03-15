c180c4c56c43e6a6e573f9660b298a87f0f8b7fd
Steve Kelly <kd2cca@gmail.com>
2023-03-15 16:40:53 -0400

    only run SnoopPrecompile on 1.9

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,5 +1,9 @@
-@precompile_setup begin
-    @precompile_all_calls begin
-        FUSE.warmup()
+if VERSION > v"1.8"
+
+    @precompile_setup begin
+        @precompile_all_calls begin
+            FUSE.warmup()
+        end
     end
+
 end
