edeaeb092e4329f04950c4cd2df85df1d6657666
Orso Meneghini <orso82@gmail.com>
2023-03-19 23:45:14 -0700

    Always use 5 PF coils as FPP default

--- cases/FPP.jl
+++ cases/FPP.jl
@@ -45,11 +45,7 @@ function case_parameters(::Type{Val{:FPP}}; version::Symbol, init_from::Symbol):
 
     ini.pf_active.n_oh_coils = 6
     ini.pf_active.n_pf_coils_inside = 0
-    if init_from == :ods
-        ini.pf_active.n_pf_coils_outside = 8
-    else
-        ini.pf_active.n_pf_coils_outside = 5
-    end
+    ini.pf_active.n_pf_coils_outside = 5
 
     ini.material.shield = "Tungsten"
     ini.material.blanket = "lithium-lead"
