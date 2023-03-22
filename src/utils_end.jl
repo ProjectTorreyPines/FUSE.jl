72d9eb4eaecba45ba4ac957d8d994e5d40be1630
Orso Meneghini <orso82@gmail.com>
2023-03-22 15:38:05 -0700

    more plots to digest

--- src/utils_end.jl
+++ src/utils_end.jl
@@ -164,15 +164,16 @@ function load(savedir::AbstractString; load_dd::Bool=true, load_ini::Bool=true, 
 end
 
 """
-    digest(dd::IMAS.dd; terminal_width::Int=140)
+    digest(dd::IMAS.dd; terminal_width::Int=136)
 
 Provides concise and informative summary of `dd`, including several plots.
 """
-function digest(dd::IMAS.dd; terminal_width::Int=140)
+function digest(dd::IMAS.dd; terminal_width::Int=136)
     #NOTE: this function is defined in FUSE and not IMAS because it uses Plots.jl and not BaseRecipies.jl
 
     IMAS.print_tiled(extract(dd); terminal_width)
 
+    # equilibrium with build and PFs
     p = plot(dd.equilibrium, legend=false)
     if !isempty(dd.build.layer)
         plot!(p[1], dd.build, legend=false)
@@ -182,9 +183,21 @@ function digest(dd::IMAS.dd; terminal_width::Int=140)
     end
     display(p)
 
+    # core profiles
     display(plot(dd.core_profiles))
 
+    # core sources
     display(plot(dd.core_sources))
+
+    # center stack stresses
+    if !ismissing(dd.solid_mechanics.center_stack.grid, :r_oh)
+        display(plot(dd.solid_mechanics.center_stack.stress))
+    end
+
+    # neutron wall loading
+    if !isempty(dd.neutronics.time_slice)
+        display(plot(dd.neutronics.time_slice[].wall_loading, x_lim=[0,Inf]))
+    end
 
     return nothing
 end
