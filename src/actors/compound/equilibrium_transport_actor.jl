82e1b3519a2c3921b79017028db1db8a03afc2f3
Orso Meneghini <orso82@gmail.com>
2023-04-10 15:50:43 -0700

    avoid copy of parameters in ActorEquilibriumTransport

--- src/actors/compound/equilibrium_transport_actor.jl
+++ src/actors/compound/equilibrium_transport_actor.jl
@@ -30,8 +30,8 @@ Compound actor that runs the following actors in succesion:
     Stores data in `dd.equilibrium, dd.core_profiles, dd.core_sources`
 """
 function ActorEquilibriumTransport(dd::IMAS.dd, act::ParametersAllActors; kw...)
-    par = act.ActorEquilibriumTransport(kw...)
-    actor = ActorEquilibriumTransport(dd, par, act)
+    par = act.ActorEquilibriumTransport
+    actor = ActorEquilibriumTransport(dd, par, act; kw...)
     step(actor)
     finalize(actor)
     return actor
