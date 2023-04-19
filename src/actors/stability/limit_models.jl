5ef52b36689e901c459d218a2a37cdcb144a97c1
Orso Meneghini <orso82@gmail.com>
2023-04-19 00:38:35 -0700

    make use of new resize! by symbol

--- src/actors/stability/limit_models.jl
+++ src/actors/stability/limit_models.jl
@@ -15,41 +15,37 @@ function run_stability_models(dd::IMAS.dd, model_names::Vector{Symbol})
 end
 
 ##### MODEL COLLECTIONS #####
-function default(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:default]
-    model = resize!(dd.stability.collection, "identifier.index" => model_index)
-    model.identifier.name = "Default"
-    model.identifier.description = "Uses the default set of models"
+function default_limits(dd::IMAS.dd)
+    collection = resize!(dd.stability.collection, :default_limits)
+    collection.identifier.name = "Default Limits"
+    collection.identifier.description = "Uses the default set of models"
 
     models = [:beta_troyon_1984, :model_201, :model_301, :model_401]
     run_stability_models(dd, models)
 end
 
 function beta_limits(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:beta_limits]
-    model = resize!(dd.stability.collection, "identifier.index" => model_index)
-    model.identifier.name = "Beta Limits"
-    model.identifier.description = "Checks all beta limit models"
+    collection = resize!(dd.stability.collection, :beta_limits)
+    collection.identifier.name = "Beta Limits"
+    collection.identifier.description = "Checks all beta limit models"
 
     models = [:beta_troyon_1984, :beta_troyon_1985, :beta_tuda_1985, :beta_bernard_1983, :beta_model_105]
     run_stability_models(dd, models)
 end
 
 function current_limits(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:current_limits]
-    model = resize!(dd.stability.collection, "identifier.index" => model_index)
-    model.identifier.name = "Current Limits"
-    model.identifier.description = "Checks all current limit models"
+    collection = resize!(dd.stability.collection, :current_limits)
+    collection.identifier.name = "Current Limits"
+    collection.identifier.description = "Checks all current limit models"
 
     models = [:model_201]
     run_stability_models(dd, models)
 end
 
 function density_limits(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:density_limits]
-    model = resize!(dd.stability.collection, "identifier.index" => model_index)
-    model.identifier.name = "Density Limits"
-    model.identifier.description = "Checks all density limit models"
+    collection = resize!(dd.stability.collection, :density_limits)
+    collection.identifier.name = "Density Limits"
+    collection.identifier.description = "Checks all density limit models"
 
     models = [:model_301]
     run_stability_models(dd, models)
@@ -57,7 +53,6 @@ end
 
 
 ##### BETA LIMIT MODELS #####
-
 """
     beta_troyon_1984(dd::IMAS.dd)
 
@@ -68,8 +63,7 @@ Model Formulation: Beta_{N} < 3.5
 Citation:  F Troyon et al 1984 Plasma Phys. Control. Fusion 26 209
 """
 function beta_troyon_1984(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:beta_troyon_1984]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :beta_troyon_1984)
     model.identifier.name = "Troyon 1984"
     model.identifier.description = "Beta_{N} < 3.5"
 
@@ -90,8 +84,7 @@ Model Formulation: Beta_{N} < 2.8
 Citation: 
 """
 function beta_troyon_1985(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:beta_troyon_1985]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :beta_troyon_1985)
     model.identifier.name = "Troyon 1985"
     model.identifier.description = "Beta_{N} < 2.8"
 
@@ -112,8 +105,7 @@ Model Formulation: Beta_{N} < 3.2
 Citation: 
 """
 function beta_tuda_1985(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:beta_tuda_1985]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :beta_tuda_1985)
     model.identifier.name = "Tuda 1985"
     model.identifier.description = "Beta_{N} < 3.2"
 
@@ -134,8 +126,7 @@ Model Formulation: Beta_{N} < 2.8
 Citation: 
 """
 function beta_bernard_1983(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:beta_bernard_1983]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :beta_bernard_1983)
     model.identifier.name = "Bernard 1983"
     model.identifier.description = "Beta_{N} < 4.4"
 
@@ -156,8 +147,7 @@ Model Formulation: beta_{N} / li < C_{beta}
 Citation: 
 """
 function model_105(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:model_105]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :model_105)
     model.identifier.name = "BetaLi::Troyon"
     model.identifier.description = "Beta_{N} / Li < 4.0"
 
@@ -183,8 +173,7 @@ Model Formulation: q95 < 2
 Citation: 
 """
 function model_201(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:model_201]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :model_201)
     model.identifier.name = "Standard::q95"
     model.identifier.description = "q_95 > 2.0"
 
@@ -208,8 +197,7 @@ Model Formulation: f_{GW,IMAS} < 1.0
 Citation: 
 """
 function model_301(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:model_301]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :model_301)
     model.identifier.name = "Standard::IMAS_Greenwald"
     model.identifier.description = "IMAS.greenwald_fraction < 1.0"
 
@@ -226,8 +214,7 @@ end
 ##### SHAPE LIMIT MODELS #####
 
 function model_401(dd::IMAS.dd)
-    model_index = IMAS.name_2_index(dd.stability.model)[:model_401]
-    model = resize!(dd.stability.model, "identifier.index" => model_index)
+    model = resize!(dd.stability.model, :model_401)
     model.identifier.name = "Standard elongation limit"
     model.identifier.description = "elongation < IMAS.elongation_limit"
 
