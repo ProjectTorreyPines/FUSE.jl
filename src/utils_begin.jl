4f963d2738407ceae0b7b6cf5a9a66452a14083b
Orso Meneghini <meneghini@fusion.gat.com>
2023-04-28 13:42:20 -0700

    Fix number of processors in parallel_environment

--- src/utils_begin.jl
+++ src/utils_begin.jl
@@ -177,11 +177,11 @@ function parallel_environment(cluster::String="localhost", nprocs_max::Integer=0
             if nprocs_max > 0
                 np = min(np, nprocs_max)
             end
+            np += 1
             ENV["JULIA_WORKER_TIMEOUT"] = "180"
             if Distributed.nprocs() < np
-                Distributed.addprocs(ClusterManagers.SlurmManager(np - Distributed.nprocs()), partition="ga-ird", topology=:master_worker, ntasks_per_core=1, mem_per_cpu="4G", time="99:99:99", job_name="python3-$(getpid())")
+                Distributed.addprocs(ClusterManagers.SlurmManager(np - Distributed.nprocs()), partition="ga-ird", topology=:master_worker, ntasks_per_core=1, time="99:99:99", job_name="python3-$(getpid())")
             end
-            println("Working with $(Distributed.nprocs()) distributed processes")
         else
             error("Not running on omega cluster")
         end
@@ -192,26 +192,26 @@ function parallel_environment(cluster::String="localhost", nprocs_max::Integer=0
             if nprocs_max > 0
                 np = min(np, nprocs_max)
             end
+            np += 1
             ENV["JULIA_WORKER_TIMEOUT"] = "180"
             if Distributed.nprocs() < np
                 Distributed.addprocs(ClusterManagers.SlurmManager(np - Distributed.nprocs()), exclusive="", topology=:master_worker, kw...)
             end
-            println("Working with $(Distributed.nprocs()) distributed processes")
         else
             error("Not running on saga cluster")
         end
 
     elseif cluster == "localhost"
-        np = length(Sys.cpu_info())
+        np = length(Sys.cpu_info()) + 1
         if nprocs_max > 0
             np = min(np, nprocs_max)
         end
-        if Distributed.nprocs() < np + 1
-            Distributed.addprocs(np - Distributed.nprocs() + 1, topology=:master_worker)
+        if Distributed.nprocs() < np
+            Distributed.addprocs(np - Distributed.nprocs(), topology=:master_worker)
         end
-        println("Working with $(Distributed.nprocs()-1) processes on $(gethostname())")
 
     else
         error("Cluster $server is unknown. Add it to the FUSE.parallel_environment")
     end
+    println("Working with $(Distributed.nprocs()-1) processes on $(gethostname())")
 end
