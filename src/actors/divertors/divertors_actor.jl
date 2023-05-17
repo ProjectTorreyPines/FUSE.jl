5acf5db18bff1f913941e98068a6d092d619a289
Orso Meneghini <orso82@gmail.com>
2023-05-17 13:18:27 -0700

    Full hook up DivertorHeatFluxModel in FUSE

--- src/actors/divertors/divertors_actor.jl
+++ src/actors/divertors/divertors_actor.jl
@@ -1,18 +1,23 @@
 #= ============== =#
 #  ActorDivertors  #
 #= ============== =#
+import BoundaryPlasmaModels
+
 Base.@kwdef mutable struct FUSEparameters__ActorDivertors{T} <: ParametersActor where {T<:Real}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
-    thermal_power_extraction_efficiency::Entry{T} = Entry(T, "-",
-        "Fraction of thermal power that is carried out by the coolant at the divertor interface, rather than being lost in the surrounding strutures.";
-        default=1.0)
+    heat_flux_model::Switch{Symbol} = Switch(Symbol, [:lengyel], "-", "Divertor heat flux model"; default=:lengyel)
+    impurities::Entry{Vector{Symbol}} = Entry(Vector{Symbol}, "-", "Vector of impurity species"; default=[])
+    impurities_fraction::Entry{Vector{T}} = Entry(Vector{T}, "-", "Vector of impurity fractions"; default=[])
+    heat_spread_factor::Entry{T} = Entry(T, "-", "Heat flux expansion factor in the private flux region (eg. due to transport) should be >= 1.0"; default=1.0)
+    thermal_power_extraction_efficiency::Entry{T} = Entry(T, "-", "Fraction of thermal power that is carried out by the coolant at the divertor interface, rather than being lost in the surrounding strutures."; default=1.0)
+    verbose::Entry{Bool} = Entry(Bool, "-", "Verbose"; default=false)
 end
 
 mutable struct ActorDivertors <: ReactorAbstractActor
     dd::IMAS.dd
     par::FUSEparameters__ActorDivertors
-    thermal_power_extraction_efficiency::Real
+    boundary_plasma_models::Vector{BoundaryPlasmaModels.DivertorHeatFluxModel}
 end
 
 """
@@ -33,38 +38,109 @@ end
 function ActorDivertors(dd::IMAS.dd, par::FUSEparameters__ActorDivertors; kw...)
     logging_actor_init(ActorDivertors)
     par = par(kw...)
-    return ActorDivertors(dd, par, par.thermal_power_extraction_efficiency)
+    return ActorDivertors(dd, par, Vector{BoundaryPlasmaModels.DivertorHeatFluxModel}())
 end
 
 function _step(actor::ActorDivertors)
     dd = actor.dd
+    par = actor.par
 
-    empty!(dd.divertors)
+    eqt = dd.equilibrium.time_slice[]
+    cp1d = dd.core_profiles.profiles_1d[]
 
-    divertors = [structure for structure in dd.build.structure if structure.type == Int(_divertor_)]
-    resize!(dd.divertors.divertor, length(divertors))
+    hfs_sol, lfs_sol = IMAS.sol(eqt, dd.wall, levels=2)
+    Psol = IMAS.power_sol(dd.core_sources, cp1d)
+    λ_omp = IMAS.widthSOL_eich(eqt, Psol)
+    flux_expansion = IMAS.flux_expansion(lfs_sol)
+    sol1 = lfs_sol[1]
 
-    total_power_incident = IMAS.total_power_source(IMAS.total_sources(dd.core_sources, dd.core_profiles.profiles_1d[]))
-    dd.divertors.time = dd.core_sources.time
-    for (k, structure) in enumerate(divertors)
+    identifiers = IMAS.identify_strike_surface(sol1, dd.divertors)
+    identifiers = [(k_divertor, k_target) for (k_divertor, k_target, k_tile) in identifiers]
 
-        div = dd.divertors.divertor[k]
-        div.name = structure.name
+    for (k_divertor, divertor) in enumerate(dd.divertors.divertor)
+        for (k_target, target) in enumerate(divertor.target)
 
-        power_incident = @ddtime(div.power_incident.data = total_power_incident ./ length(dd.divertors.divertor))
+            id = (k_divertor, k_target)
+            if id == identifiers[1]
+                strike_index = 1
+            elseif id == identifiers[2]
+                strike_index = length(sol1.r)
+            else
+                continue
+            end
 
-        power_neutrals = @ddtime(div.power_neutrals.data = 0.0)
+            # Power conducted/convected by the plasma on this divertor target  [W]
+            @ddtime(target.power_conducted.data = Psol / 2.0)
+            @ddtime(target.power_convected.data = 0.0)
 
-        power_radiated = @ddtime(div.power_radiated.data = 0.0)
+            # λq at the outer midplane [m]
+            resize!(target.two_point_model)
+            target.two_point_model[].sol_heat_decay_length = λ_omp
 
-        power_recombination_neutrals = @ddtime(div.power_recombination_neutrals.data = 0.0)
+            # target flux expansion [-] and wetted area [m^2]
+            flxexp = @ddtime(target.flux_expansion.data = flux_expansion[strike_index == 1 ? 1 : 2])
+            λ_target = flxexp * λ_omp
+            wetted_area = @ddtime(target.wetted_area.data = λ_target * 2π * sol1.r[strike_index])
 
-        @ddtime(div.power_thermal_extracted.data = actor.thermal_power_extraction_efficiency * (
-            power_incident +
-            power_radiated +
-            power_neutrals +
-            power_recombination_neutrals))
+            # strike angles [rad]
+            @ddtime(target.tilt_angle_tor.data = atan(sol1.Bp[strike_index] / sol1.Bt[strike_index]))
+            @ddtime(target.tilt_angle_pol.data = sol1.strike_angles[strike_index == 1 ? 1 : 2])
+
+            # Setup model based on dd and run
+            boundary_plasma_model = BoundaryPlasmaModels.DivertorHeatFluxModel(par.heat_flux_model)
+            push!(actor.boundary_plasma_models, boundary_plasma_model)
+            setup_model(boundary_plasma_model, target, eqt, cp1d, sol1; par.impurities, par.impurities_fraction, par.heat_spread_factor)
+            boundary_plasma_model()
+            if par.verbose
+                println("      == $(uppercase(target.name)) ==")
+                BoundaryPlasmaModels.show_summary(boundary_plasma_model)
+                println()
+            end
+
+            # get the peak power heatflux [W.m^2]
+            power_flux_peak = @ddtime(target.power_flux_peak.data = boundary_plasma_model.results.q_perp_target_spread)
+
+            # get the total power on the divertor [W]
+            power_incident = power_flux_peak * wetted_area
+            @ddtime(target.power_incident.data = power_incident)
+        end
+
+        @ddtime(divertor.power_thermal_extracted.data = par.thermal_power_extraction_efficiency * @ddtime(divertor.power_incident.data))
     end
 
     return actor
+end
+
+function setup_model(
+    boundary_plasma_model::BoundaryPlasmaModels.LengyelModel,
+    target::IMAS.divertors__divertor___target,
+    eqt::IMAS.equilibrium__time_slice,
+    cp1d::IMAS.core_profiles__profiles_1d,
+    sol1::IMAS.OpenFieldLine;
+    impurities::Vector{Symbol},
+    impurities_fraction::Vector{<:Real},
+    heat_spread_factor::Real=1.0)
+
+    @assert (length(impurities) == length(impurities_fraction))
+
+    boundary_plasma_model.parameters.plasma.P_SOL = @ddtime(target.power_conducted.data) + @ddtime(target.power_convected.data)
+    boundary_plasma_model.parameters.plasma.R_omp = sol1.r[sol1.midplane_index]
+    boundary_plasma_model.parameters.plasma.Ip = eqt.global_quantities.ip
+    boundary_plasma_model.parameters.plasma.κ = eqt.boundary.elongation
+    boundary_plasma_model.parameters.plasma.ϵ = eqt.boundary.minor_radius / eqt.boundary.geometric_axis.r
+    boundary_plasma_model.parameters.plasma.Bt_omp = sol1.Bt[sol1.midplane_index]
+    boundary_plasma_model.parameters.plasma.Bpol_omp = sol1.Bp[sol1.midplane_index]
+
+    boundary_plasma_model.parameters.sol.n_up = cp1d.electrons.density_thermal[end]
+    boundary_plasma_model.parameters.sol.T_up = cp1d.electrons.temperature[end]
+    boundary_plasma_model.parameters.sol.λ_omp = target.two_point_model[].sol_heat_decay_length
+    boundary_plasma_model.parameters.sol.imp = impurities
+    boundary_plasma_model.parameters.sol.f_imp = impurities_fraction
+
+    boundary_plasma_model.parameters.target.f_omp2target_expansion = @ddtime(target.flux_expansion.data)
+    λ_target = boundary_plasma_model.parameters.sol.λ_omp * boundary_plasma_model.parameters.target.f_omp2target_expansion
+    boundary_plasma_model.parameters.target.R = @ddtime(target.wetted_area.data) / (λ_target * 2π)
+    boundary_plasma_model.parameters.target.f_spread_pfr = heat_spread_factor
+    boundary_plasma_model.parameters.target.α_sp = @ddtime(target.tilt_angle_tor.data)
+    boundary_plasma_model.parameters.target.θ_sp = @ddtime(target.tilt_angle_pol.data)
 end
