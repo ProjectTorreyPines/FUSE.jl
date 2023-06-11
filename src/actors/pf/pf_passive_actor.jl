8b318466a4465aebdc53a9ab9d5cecbb4086e4bb
Orso Meneghini <orso82@gmail.com>
2023-06-10 23:44:56 -0700

    pf_passive now include identifier linked to build.layer
    mention #339

--- src/actors/pf/pf_passive_actor.jl
+++ src/actors/pf/pf_passive_actor.jl
@@ -48,19 +48,19 @@ function _step(actor::ActorPassiveStructures)
         end
         if all(l1.type != structure.type for structure in dd.build.structure)
             poly = IMAS.join_outlines(l.outline.r, l.outline.z, l1.outline.r, l1.outline.z)
-            add_pf_passive_loop(dd.pf_passive, l1.name, poly[1], poly[2])
+            add_pf_passive_loop(dd.pf_passive, l1, poly[1], poly[2])
         end
     end
 
     # then all structures
     for structure in dd.build.structure
-        add_pf_passive_loop(dd.pf_passive, structure.name, structure.outline.r, structure.outline.z)
+        add_pf_passive_loop(dd.pf_passive, structure)
     end
 
     # OH and plug
-    add_pf_passive_loop(dd.pf_passive, dd.build.layer[2].name, dd.build.layer[2].outline.r, dd.build.layer[2].outline.z)
+    add_pf_passive_loop(dd.pf_passive, dd.build.layer[2])
     if dd.build.layer[1].material != "Vacuum"
-        add_pf_passive_loop(dd.pf_passive, dd.build.layer[1].name, dd.build.layer[1].outline.r, dd.build.layer[1].outline.z)
+        add_pf_passive_loop(dd.pf_passive, dd.build.layer[1])
     end
 
     # cryostat
@@ -73,15 +73,31 @@ function _step(actor::ActorPassiveStructures)
         i = argmin(abs.(layer.outline.r))
         r = vcat(layer.outline.r[i+1:end-1], layer.outline.r[1:i], reverse(r), layer.outline.r[i+1:i+1])
         z = vcat(layer.outline.z[i+1:end-1], layer.outline.z[1:i], reverse(z), layer.outline.z[i+1:i+1])
-        add_pf_passive_loop(dd.pf_passive, dd.build.layer[end].name, r, z)
+        add_pf_passive_loop(dd.pf_passive, dd.build.layer[end], r, z)
     end
 
     return actor
 end
 
-function add_pf_passive_loop(pf_passive::IMAS.pf_passive, name::AbstractString, r::AbstractVector{T}, z::AbstractVector{T}) where {T<:Real}
+function add_pf_passive_loop(pf_passive::IMAS.pf_passive, layer::IMAS.build__layer)
+    return add_pf_passive_loop(pf_passive, layer, layer.outline.r, layer.outline.z)
+end
+
+function add_pf_passive_loop(pf_passive::IMAS.pf_passive, layer::IMAS.build__layer, r::Vector{T}, z::Vector{T}) where {T<:Real}
+    return add_pf_passive_loop(pf_passive, layer.name, "layer $(IMAS.index(layer))", r, z)
+end
+
+function add_pf_passive_loop(pf_passive::IMAS.pf_passive, structure::IMAS.build__structure)
+    return add_pf_passive_loop(pf_passive, structure.name, "structure $(IMAS.index(structure))", structure.outline.r, structure.outline.z)
+end
+
+function add_pf_passive_loop(pf_passive::IMAS.pf_passive, name::AbstractString, identifier::AbstractString, r::AbstractVector{T}, z::AbstractVector{T}) where {T<:Real}
     resize!(resize!(pf_passive.loop, length(pf_passive.loop) + 1)[end].element, 1)
     pf_passive.loop[end].name = replace(name, r"[lh]fs " => "")
     pf_passive.loop[end].element[end].geometry.outline.r = r
     pf_passive.loop[end].element[end].geometry.outline.z = z
+    if !isempty(identifier)
+        pf_passive.loop[end].element[end].identifier = identifier
+    end
+    return pf_passive.loop[end]
 end
