02efe7bc54b53ce199e295b0fcfaba3a6d9b120d
Orso Meneghini <orso82@gmail.com>
2023-07-28 13:26:00 -0700

    rename ActorEquilibriumTransport --> ActorStationaryPlasma

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -102,7 +102,7 @@ include(joinpath("actors", "costing", "gasc_costing_actor.jl"))
 include(joinpath("actors", "costing", "costing_actor.jl"))
 
 # NOTE: compound actors should be defined last
-include(joinpath("actors", "compound", "equilibrium_transport_actor.jl"))
+include(joinpath("actors", "compound", "stationary_plasma_actor.jl"))
 include(joinpath("actors", "compound", "whole_facility_actor.jl"))
 
 #= ========== =#
