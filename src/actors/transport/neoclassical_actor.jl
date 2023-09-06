4cc2c34f07dc00aaaeb0f61704a82395255e86e0
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2023-09-06 13:11:16 -0700

    change neoclassical_model to model

--- src/actors/transport/neoclassical_actor.jl
+++ src/actors/transport/neoclassical_actor.jl
@@ -6,7 +6,7 @@ import NEO
 Base.@kwdef mutable struct FUSEparameters__ActorNeoclassical{T} <: ParametersActor where {T<:Real}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
-    neoclassical_model::Switch{Symbol} = Switch{Symbol}([:changhinton, :neo], "-", "Neoclassical model to run"; default=:changhinton)
+    model::Switch{Symbol} = Switch{Symbol}([:changhinton, :neo], "-", "Neoclassical model to run"; default=:changhinton)
     rho_transport::Entry{AbstractVector{<:T}} = Entry{AbstractVector{<:T}}("-", "rho_tor_norm values to compute neoclassical fluxes on"; default=0.2:0.1:0.8)
 end
 
@@ -46,11 +46,11 @@ function _step(actor::ActorNeoclassical)
     m1d.grid_flux.rho_tor_norm = par.rho_transport
     cp1d = dd.core_profiles.profiles_1d[]
 
-    if par.neoclassical_model == :changhinton
+    if par.model == :changhinton
         model.identifier.name = "Chang-Hinton"
         eqt = dd.equilibrium.time_slice[]
         actor.flux_solutions = [NEO.changhinton(eqt, cp1d, rho, 1) for rho in par.rho_transport]
-    elseif par.neoclassical_model == :neo 
+    elseif par.model == :neo 
         model.identifier.name = "NEO"
         rho_cp = cp1d.grid.rho_tor_norm
         gridpoint_cp = [argmin(abs.(rho_cp .- rho)) for rho in par.rho_transport]
@@ -77,9 +77,6 @@ function _step(actor::ActorNeoclassical)
             solution = IMAS.flux_solution(particle_flux_electrons, 0.0, energy_flux_electrons, total_ion_energy_flux)
             push!(actor.flux_solutions, solution)
         end
-  
-    else
-        error("$(par.neoclassical_model) is not implemented")
     end
 
     return actor
@@ -98,16 +95,20 @@ function _finalize(actor::ActorNeoclassical)
 
     model = findfirst(:neoclassical, actor.dd.core_transport.model)
     m1d = model.profiles_1d[]
+
+    if par.model == :neo
+        m1d.electrons.particles.flux = zeros(length(par.rho_transport))
+        m1d.electrons.energy.flux = zeros(length(par.rho_transport))
+    end
+
     m1d.total_ion_energy.flux = zeros(length(par.rho_transport))
-    m1d.electrons.particles.flux = zeros(length(par.rho_transport))
-    m1d.electrons.energy.flux = zeros(length(par.rho_transport))
 
     for (neoclassical_idx, rho) in enumerate(par.rho_transport)
         rho_transp_idx = findfirst(i -> i == rho, m1d.grid_flux.rho_tor_norm)
         rho_cp_idx = argmin(abs.(cp1d.grid.rho_tor_norm .- rho))
         m1d.total_ion_energy.flux[rho_transp_idx] = actor.flux_solutions[neoclassical_idx].ENERGY_FLUX_i * IMAS.gyrobohm_energy_flux(cp1d, eqt)[rho_cp_idx] # W / m^2
 
-        if par.neoclassical_model == :neo
+        if par.model == :neo
             m1d.electrons.particles.flux[rho_transp_idx] = actor.flux_solutions[neoclassical_idx].PARTICLE_FLUX_e * IMAS.gyrobohm_particle_flux(cp1d, eqt)[rho_cp_idx]
             m1d.electrons.energy.flux[rho_transp_idx] = actor.flux_solutions[neoclassical_idx].ENERGY_FLUX_e * IMAS.gyrobohm_energy_flux(cp1d, eqt)[rho_cp_idx]
         end
