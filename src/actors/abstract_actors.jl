fb7d79453110d7e0930780f34782f6fe0a22c948
Orso Meneghini <orso82@gmail.com>
2023-09-06 21:45:46 -0700

    ActorNoOP
    close #409

--- src/actors/abstract_actors.jl
+++ src/actors/abstract_actors.jl
@@ -46,10 +46,6 @@ function step(actor::T, args...; kw...) where {T<:AbstractActor}
     return actor
 end
 
-function step(actor::Missing)
-    return actor
-end
-
 #= ======== =#
 #  finalize  #
 #= ======== =#
@@ -79,10 +75,6 @@ function finalize(actor::T) where {T<:AbstractActor}
         end
     end
     memory_time_tag("$(name(actor)) - finalize OUT")
-    return actor
-end
-
-function finalize(actor::Missing)
     return actor
 end
 
