0799a8ff53d0529add858c59c7f00e68c032bea1
Orso Meneghini <orso82@gmail.com>
2023-09-15 10:15:32 -0700

    add missing controller_actor include

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -104,6 +104,8 @@ include(joinpath("actors", "costing", "aries_costing_actor.jl"))
 include(joinpath("actors", "costing", "gasc_costing_actor.jl"))
 include(joinpath("actors", "costing", "costing_actor.jl"))
 
+include(joinpath("actors", "control", "controller_actor.jl"))
+
 # NOTE: compound actors should be defined last
 include(joinpath("actors", "compound", "stationary_plasma_actor.jl"))
 include(joinpath("actors", "compound", "dynamic_plasma_actor.jl"))
