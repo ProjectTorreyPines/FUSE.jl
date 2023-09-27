6de014032a77529c4f358ada6f28b87a8c0721a1
Orso Meneghini <orso82@gmail.com>
2023-09-26 21:55:04 -0700

    more refinements to DTT case
    mention @ggdose

--- cases/DTT.jl
+++ cases/DTT.jl
@@ -10,70 +10,50 @@ function case_parameters(::Type{Val{:DTT}})::Tuple{ParametersAllInits,Parameters
     ini.general.init_from = :scalars
 
     ini.build.symmetric = true
+    ini.build.divertors = :double
     ini.material.wall = "Tungsten"
     ini.material.shield = "Steel, Stainless 316"
 
     ini.equilibrium.B0 = 5.85
     ini.equilibrium.ip = 5.5e6
-    ini.equilibrium.pressure_core = 0.9e6 
 
-    ini.equilibrium.xpoints = :lower
+    act.ActorStabilityLimits.raise_on_breach = false
+    ini.equilibrium.pressure_core = 0.9e6
+
+    ini.equilibrium.xpoints = :double
+    act.ActorEquilibrium.symmetrize = true
+
     ini.equilibrium.boundary_from = :scalars
-
-    R0 = 2.19
-    Z0 = 0.0
-    ϵ = 0.32
-    κ = 1.65
-    δ = 0.35
-    ζ = -0.09583 #lasciato quello di iter
-
-    ini.equilibrium.R0 = R0
-    ini.equilibrium.Z0 = Z0
-    ini.equilibrium.ϵ = ϵ
-    ini.equilibrium.κ = κ
-    ini.equilibrium.δ = δ
-    ini.equilibrium.ζ = ζ
+    ini.equilibrium.R0 = 2.19
+    ini.equilibrium.Z0 = 0.0
+    ini.equilibrium.ϵ = 0.32
+    ini.equilibrium.κ = 1.65
+    ini.equilibrium.δ = 0.45
+    ini.equilibrium.ζ = -0.1
+    ini.equilibrium.𝚶 = 0.0
 
     # explicitly set thickness of radial build layers
+    # ==============
     ini.build.layers = layers = OrderedCollections.OrderedDict{Symbol,Float64}()
-    layers[:gap_OH] = 0.45 # 0.45
-    layers[:OH] = 0.325  # 0.775
-    layers[:hfs_TF] = 0.46 #1.235
+    layers[:gap_OH] = 0.45
+    layers[:OH] = 0.325
 
-    # layers[:hfs_vacuum_vessel] = 0.15,
-    # layers[:hfs_shield] = 0.05
-#roba da FPP
-    layers[:hfs_gap_low_temp_shield_TF] = 0.01
+    layers[:hfs_TF] = 0.46
     layers[:hfs_low_temp_shield] = 0.02
-    layers[:hfs_gap_vacuum_vessel_low_temp_shield] = 0.01 #1.275
-
-    # raggio vessel int = 1.275
-    layers[:hfs_vacuum_vessel_wall_outer] = 0.01
     layers[:hfs_vacuum_vessel] = 0.115
-    layers[:hfs_vacuum_vessel_wall_inner] = 0.01 #1.410
-#roba da ITER.jl
-    layers[:hfs_wall] = 0.05 #1.460
-    layers[:hfs_gap_plasma_wall]= 0.03 #1.490
-    layers[:plasma] = 1.4 # R0 2.19 - 2.890
-    layers[:lfs_gap_wall_plasma]= 0.078 # 2.968
-    layers[:lfs_wall] = 0.255 #tiene conto pure del vuoto vv/fw - 3.223
+    layers[:hfs_wall] = 0.05
 
-    # layers[:lfs_shield] = 0.05
-    # layers[:lfs_vacuum_vessel] = 0.20
+    layers[:plasma] = 1.4
 
-#roba da FPP
-    layers[:lfs_vacuum_vessel_wall_inner] = 0.01
+    layers[:lfs_wall] = 0.255
     layers[:lfs_vacuum_vessel] = 0.21
-    layers[:lfs_vacuum_vessel_wall_outer] = 0.01 # 3.453
-
-    layers[:lfs_gap_vacuum_vessel_low_temp_shield] = 0.01 # 3.457
     layers[:lfs_low_temp_shield] = 0.02
-    layers[:lfs_gap_low_temp_shield_TF] = 0.01
-
     layers[:lfs_TF] = 0.585 # 4.078
+
     layers[:gap_cryostat] = 0.5
-    layers[:cryostat] = 0.30
-    ini.build.n_first_wall_conformal_layers = 1
+    layers[:cryostat] = 0.10
+    # ==============
+    ini.build.n_first_wall_conformal_layers = 2
 
     ini.oh.n_coils = 6
     ini.pf_active.n_coils_inside = 0
@@ -103,10 +83,10 @@ function case_parameters(::Type{Val{:DTT}})::Tuple{ParametersAllInits,Parameters
 
     ini.core_profiles.ejima = 0.4
 
-    ini.nbi.power_launched = 10e6         
-    ini.nbi.beam_energy = 0.5e6                   #500 keV
-    ini.ec_launchers.power_launched = 29e6        #of 32 installed
-    ini.ic_antennas.power_launched = 6e6          #of 8 installed
+    ini.nbi.power_launched = 10e6
+    ini.nbi.beam_energy = 0.5e6
+    ini.ec_launchers.power_launched = 29e6 #of 32 installed
+    ini.ic_antennas.power_launched = 6e6   #of 8 installed
 
     act.ActorFluxMatcher.evolve_densities = Dict(
         :Ne => :match_ne_scale,
