075cea042d3b00920b4259abe417bc28951fd318
Orso Meneghini <orso82@gmail.com>
2023-12-30 00:06:28 -0800

    Add ina/act to YAML as tests

--- test/runtests_workflows.jl
+++ test/runtests_workflows.jl
@@ -16,42 +16,42 @@ end
     FUSE.ActorFluxMatcher(dd, act)
 end
 
-@testset "init" begin
+@testset "use_cases" begin
     for (testname, (args, kw)) in FUSE.test_cases
         @testset "$testname" begin
             FUSE.TimerOutputs.reset_timer!(FUSE.timer, testname)
             FUSE.TimerOutputs.@timeit FUSE.timer "$testname" begin
                 println("== $(testname) ==")
                 ini, act = FUSE.case_parameters(args...; kw...)
-                FUSE.init(ini, act)
-            end
-        end
-    end
-end
+                dd = IMAS.dd()
 
-@testset "yaml_ini_act" begin
-    for (testname, (args, kw)) in FUSE.test_cases
-        @testset "$testname" begin
-            FUSE.TimerOutputs.reset_timer!(FUSE.timer, testname)
-            FUSE.TimerOutputs.@timeit FUSE.timer "$testname" begin
-                println("== $(testname) ==")
-                ini, act = FUSE.case_parameters(args...; kw...)
-
-                ini_str = FUSE.SimulationParameters.par2ystr(ini; skip_defaults=true, show_info=false)
-                ini2 = try
-                    FUSE.SimulationParameters.ystr2par(ini_str, FUSE.ParametersInits())
-                catch e
-                    println(ini_str)
-                    rethrow(e)
+                @testset "ini_yaml" begin
+                    ini_str = FUSE.SimulationParameters.par2ystr(ini; skip_defaults=true, show_info=false)
+                    ini2 = try
+                        FUSE.SimulationParameters.ystr2par(ini_str, FUSE.ParametersInits())
+                    catch e
+                        println(ini_str)
+                        rethrow(e)
+                    end
                 end
 
-                act_str = FUSE.SimulationParameters.par2ystr(act; skip_defaults=true, show_info=false)
-                act2 = try
-                    FUSE.SimulationParameters.ystr2par(act_str, FUSE.ParametersActors())
-                catch e
-                    println(act_str)
-                    rethrow(e)
+                @testset "act_yaml" begin
+                    act_str = FUSE.SimulationParameters.par2ystr(act; skip_defaults=true, show_info=false)
+                    act2 = try
+                        FUSE.SimulationParameters.ystr2par(act_str, FUSE.ParametersActors())
+                    catch e
+                        println(act_str)
+                        rethrow(e)
+                    end
                 end
+
+                @testset "init" begin
+                    FUSE.init(dd, ini, act)
+                end
+
+                # @testset "whole_facility" begin
+                #     FUSE.ActorWholeFacility(dd, act)
+                # end
             end
         end
     end
