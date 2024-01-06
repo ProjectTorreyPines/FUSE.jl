f74e164ac2da534b7cc69f654eb5d5ec9db6dfc5
daveweisberg <weisbergd@fusion.gat.com>
2024-01-05 17:04:45 -0800

    Edit LFSsizing actor.

--- src/actors/build/lfs_actor.jl
+++ src/actors/build/lfs_actor.jl
@@ -45,26 +45,33 @@ function _step(actor::ActorLFSsizing)
     dd = actor.dd
     par = actor.par
 
+    # calculate TF leg radius that gives required TF field ripple
     new_TF_radius = IMAS.R_tf_ripple(IMAS.get_build_layer(dd.build.layer, type=_plasma_).end_radius, dd.build.tf.ripple, dd.build.tf.coils_n)
 
-    itf = IMAS.get_build_index(dd.build.layer, type=_tf_, fs=_lfs_) - 1
-    iplasma = IMAS.get_build_index(dd.build.layer, type=_plasma_) + 1
+    # calculate vertical maintenance port geometry
+    rVP_hfs_ib, rVP_hfs_ob, rVP_lfs_ib, rVP_lfs_ob = IMAS.vertical_maintenance(dd.build)
 
     # resize layers proportionally
     # start from the vacuum gaps before resizing the material layers
-    for vac in (true, false)
-        old_TF_radius = IMAS.get_build_layer(dd.build.layer, type=_tf_, fs=_lfs_).start_radius
-        delta = new_TF_radius - old_TF_radius
-        if par.verbose
-            println("TF outer leg radius changed by $delta [m]")
-        end
-        thicknesses = [dd.build.layer[k].thickness for k in iplasma:itf if !vac || lowercase(dd.build.layer[k].material) == "vacuum"]
-        for k in iplasma:itf
-            if !vac || lowercase(dd.build.layer[k].material) == "vacuum"
-                dd.build.layer[k].thickness *= (1 + delta / sum(thicknesses))
-                hfs_thickness = IMAS.get_build_layer(dd.build.layer, identifier=dd.build.layer[k].identifier, fs=_hfs_).thickness
-                if dd.build.layer[k].thickness < hfs_thickness
-                    dd.build.layer[k].thickness = hfs_thickness
+    old_TF_radius = IMAS.get_build_layer(dd.build.layer, type=_tf_, fs=_lfs_).start_radius
+    delta = new_TF_radius - old_TF_radius
+    if par.verbose
+        println("TF outer leg radius changed by $delta [m]")
+    end
+
+    # only change LFS layers if TF needs to be moved outward
+    itf = IMAS.get_build_index(dd.build.layer, type=_tf_, fs=_lfs_) - 1
+    iplasma = IMAS.get_build_index(dd.build.layer, type=_plasma_) + 1
+    if new_TF_radius > old_TF_radius
+        for vac in (true, false)
+            thicknesses = [dd.build.layer[k].thickness for k in iplasma:itf if !vac || lowercase(dd.build.layer[k].material) == "vacuum"]
+            for k in iplasma:itf
+                if !vac || lowercase(dd.build.layer[k].material) == "vacuum"
+                    dd.build.layer[k].thickness *= (1 + delta / sum(thicknesses))
+                    hfs_thickness = IMAS.get_build_layer(dd.build.layer, identifier=dd.build.layer[k].identifier, fs=_hfs_).thickness
+                    if dd.build.layer[k].thickness < hfs_thickness
+                        dd.build.layer[k].thickness = hfs_thickness
+                    end
                 end
             end
         end
