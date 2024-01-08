c7fafb106edb653e6d4e2f96bec8f30580a4a848
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2024-01-08 10:35:16 -0800

    create parameters_workflow backend

--- src/parameters_workflows.jl
+++ src/parameters_workflows.jl
@@ -1,19 +1,27 @@
 abstract type ParametersFlow <: AbstractParameters end
 abstract type AbstractWorkflow end
 
-Base.@kwdef mutable struct ParametersFlowSettings{T} <: ParametersFlow where {T<:Real}
-    _parent::WeakRef = WeakRef(nothing)
-    _name::Symbol = :settings
-    server::Switch{Symbol} = Switch{Symbol}([:localhost, :omega, :saga], "-", "Where to run"; default=:localhost)
+function flow_common_parameters(name::Symbol)
+    if name == :server
+        return Switch{String}(["localhost", "omega", "saga"], "-", "Where to run"; default="localhost")
+    elseif name == :n_workers
+        return Entry{Int}("-", "number of workers to run with")
+    elseif name == :file_save_mode
+        return Switch{Symbol}([:safe_write,:overwrite],"-", "The policy to implement when saving files, safe_write only writen when the folder is empty, overwrite overwrites"; default=:safe_write)
+
+    end
 end
 
-Base.@kwdef mutable struct ParametersFlowTGLF_DB{T} <: ParametersFlow where {T<:Real}
+Base.@kwdef mutable struct ParametersFlowTGLFdb{T} <: ParametersFlow where {T<:Real}
     _parent::WeakRef = WeakRef(nothing)
-    _name::Symbol = :FlowTGLF_DB
-    settings::ParametersFlowSettings{T} = ParametersFlowSettings{T}()
+    _name::Symbol = :FlowTGLFdb
+    server::Switch{String} = flow_common_parameters(:server)
+    n_workers::Entry{Int} = flow_common_parameters(:n_workers)
+    file_save_mode::Switch{Symbol} = flow_common_parameters(:file_save_mode)
     sat_rules::Entry{Vector{Symbol}} = Entry{Vector{Symbol}}("-", "TGLF saturation rules to run")
-    save_folder::Entry{String} = Entry{String}("-", "folder to save the database runs into"; default="")
-    database_folder::Entry{String} = Entry{String}("-", "folder with input database"; default="")
+    save_folder::Entry{String} = Entry{String}("-", "Folder to save the database runs into")
+
+    database_folder::Entry{String} = Entry{String}("-", "Folder with input database")
 end
 
 
@@ -36,7 +44,6 @@ Returns the name of the workflow as a string
 function name(wf::AbstractWorkflow)
     return string(split(string(typeof(wf)), ".")[end])
 end
-
 
 function flow_parameters(flow::Symbol; kw...)
     if length(methods(flow_parameters, (Type{Val{flow}},))) == 0
