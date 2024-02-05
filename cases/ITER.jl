081aa80f53215940cf4620cb897870a51cce97d9
Orso Meneghini <orso82@gmail.com>
2024-02-05 01:17:04 -0800

    Add LH to ITER

--- cases/ITER.jl
+++ cases/ITER.jl
@@ -8,7 +8,7 @@ Arguments:
   - `init_from`: `:scalars` or `:ods` (ODS contains equilibrium and wall information)
 """
 function case_parameters(::Type{Val{:ITER}}; init_from::Symbol, boundary_from=:MXH_params)::Tuple{ParametersAllInits,ParametersAllActors}
-    ini = ParametersInits(; n_nb=1, n_ec=1, n_ic=1, n_pl=1)
+    ini = ParametersInits(; n_nb=1, n_ec=1, n_ic=1, n_lh=1, n_pl=1)
     act = ParametersActors()
 
     # checking init_from and boundary_from
@@ -103,8 +103,13 @@ function case_parameters(::Type{Val{:ITER}}; init_from::Symbol, boundary_from=:M
 
     ini.nb_unit[1].power_launched = t -> 16.7e6 + ramp((t - 100) / 100.0) * 16.7e6
     ini.nb_unit[1].beam_energy = 1e6
+
     ini.ec_launcher[1].power_launched = t -> 10e6 + ramp((t - 100) / 100.0) * 10e6
+
     ini.ic_antenna[1].power_launched = t -> 12e6 + ramp((t - 100) / 100.0) * 12e6
+
+    ini.lh_antenna[1].power_launched = t -> 10e6 + ramp((t - 100) / 100.0) * 10e6
+
     ini.pellet_launcher[1].shape = :cylindrical
     ini.pellet_launcher[1].species = :T
     ini.pellet_launcher[1].size = Float64[0.003, 0.004]
