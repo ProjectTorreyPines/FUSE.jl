9c3c092214530b1c3bd32f716d0427a3081cac95
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2024-03-08 16:33:26 -0800

    union -> intersect

--- test/check_init_primary_quanties.jl
+++ test/check_init_primary_quanties.jl
@@ -2,7 +2,7 @@ using FUSE
 using Test
 
 @testset "check_init_expressions" begin
-    data_fields_all = union(FUSE.init_expressions()...)
+    data_fields_all = intersect(FUSE.init_expressions()...)
     data_fields_from_file = FUSE.load_init_expressions()
     @assert isempty(symdiff(data_fields_from_file, data_fields_all)) "data fields after init have changed! run `make init_expressions`"
 end
