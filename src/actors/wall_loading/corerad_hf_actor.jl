1060bee4810729f020fb584a3dd88422f5d18b6a
Orso Meneghini <orso82@gmail.com>
2024-03-21 22:21:43 -0700

    formatting and restructuring of ActorCoreRadHeatFlux and ActorParticleHeatFlux

--- src/actors/wall_loading/corerad_hf_actor.jl
+++ src/actors/wall_loading/corerad_hf_actor.jl
@@ -2,20 +2,19 @@
 #  ActorCoreRadHeatFlux  #
 #= ===================== =#
 
-# Definiton of act parameters
 Base.@kwdef mutable struct FUSEparameters__ActorCoreRadHeatFlux{T<:Real} <: ParametersActorPlasma{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     _time::Float64 = NaN
     N::Entry{Int} = Entry{Int}("-", "Number of launched photons"; default=500000)
+    r::Entry{Vector{T}} = Entry{Vector{T}}("m", "Vector of r at outermidplane"; default=T[])
+    q::Entry{Vector{T}} = Entry{Vector{T}}("W m^-2", "Vector of parallel power density at outer midplane"; default=T[])
+    levels::Entry{Union{Int,Vector}} = Entry{Union{Int,Vector}}("-", "If Int it defines number of levels in SOL, if vector it corresponds to the psi levels to build SOL"; default=20)
+    merge_wall::Entry{Bool} = Entry{Bool}("-", "Merge dd.wall in mesh for the heat flux "; default=true)
+    step::Entry{T} = Entry{T}("m", " Step for discretization of the default wall mesh (dd.wall)"; default=0.1)
     do_plot::Entry{Bool} = act_common_parameters(; do_plot=false)
-    r::Entry{AbstractVector{Float64}} = Entry{AbstractVector{Float64}}("m", "Vector of r at outermidplane"; default = Float64[])
-    q::Entry{AbstractVector{Float64}} = Entry{AbstractVector{Float64}}("W m^-2", "Vector of parallel power density  at outer midplane"; default = Float64[])
-    merge_wall::Entry{Bool} = Entry{Bool}("-","Merge dd.wall in mesh for the heat flux "; default = true)
-    levels::Entry{Union{Int,AbstractVector}} = Entry{Union{Int,AbstractVector}}("-", "If Int it defines number of levels in SOL, if vector it corresponds to the psi levels to build SOL"; default = 20)
-    step::Entry{Real} = Entry{Real}("m"," Step for discretization of the default wall mesh (dd.wall)"; default = 0.1)
 end
-# Definition of Actor struct
+
 mutable struct ActorCoreRadHeatFlux{D,P} <: SingleAbstractActor{D,P}
     dd::IMAS.dd{D}
     par::FUSEparameters__ActorCoreRadHeatFlux{P}
@@ -27,12 +26,10 @@ mutable struct ActorCoreRadHeatFlux{D,P} <: SingleAbstractActor{D,P}
 end
 
 """
-ActorCoreRadHeatFlux
+    ActorCoreRadHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
 
 Computes the heat flux on the wall due to the core radiation
-
 """
-
 function ActorCoreRadHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
     actor = ActorCoreRadHeatFlux(dd, act.ActorCoreRadHeatFlux; kw...)
     step(actor)
@@ -40,53 +37,45 @@ function ActorCoreRadHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
     return actor
 end
 
-# Operate actor
 function _step(actor::ActorCoreRadHeatFlux)
     dd = actor.dd
     par = actor.par
 
-    do_plot::Bool = par.do_plot
-    r::AbstractVector{Float64} = par.r
-    q::AbstractVector{Float64} = par.q
-    merge_wall::Bool = par.merge_wall
-    step::Real = par.step
-    levels::Union{Int,AbstractVector} = par.levels
-    N::Int = par.N
-   
     eqt = dd.equilibrium.time_slice[]
+    cp1d = dd.core_profiles.profiles_1d[]
 
     # Compute wall mesh 
     # (Rwall, Zwall)        wall mesh (with intersections of SOL) -  m
     # s                     curvilinear abscissa computed from (Rwall, Zwall), clockwise starting at OMP - m
-    Rwall, Zwall, _, _, s, _, _, _= IMAS.mesher_HF(dd, r = r, q = q, merge_wall = merge_wall, levels = levels, step = step)
+    Rwall, Zwall, _, _, s, _, _, _ = IMAS.mesher_HF(dd; par.r, par.q, par.merge_wall, par.levels, par.step)
 
-    #Parameters for heat flux due to core radiarion
-    psi       = dd.core_sources.source[1].profiles_1d[1].grid.psi
-    source_1d = -IMAS.total_radiation_sources(dd).electrons.energy # minus sign because loss for dd.core_sources
-    Prad_core = -IMAS.total_radiation_sources(dd).electrons.power_inside[end]
+    # Parameters for heat flux due to core radiarion
+    total_rad_source1d = IMAS.total_radiation_sources(dd.core_sources, cp1d)
+    psi = total_rad_source1d.grid.psi
+    source_1d = -total_rad_source1d.electrons.energy # minus sign because loss for dd.core_sources
+    Prad_core = -total_rad_source1d.electrons.power_inside[end]
 
-    # Compute the heat flux due to the core radiation - Qrad - W/m2
-    Qrad = IMAS.core_radiation_HF(eqt, psi, source_1d, N, Rwall, Zwall, Prad_core)
+    # Compute the heat flux due to the core radiation - q_core_rad - W/m2
+    q_core_rad = IMAS.core_radiation_HF(eqt, psi, source_1d, par.N, Rwall, Zwall, Prad_core)
 
-    HF = IMAS.WallHeatFlux(r = Rwall, z = Zwall, q_core_rad = Qrad, s = s)
+    HF = IMAS.WallHeatFlux(; r=Rwall, z=Zwall, q_core_rad, s)
 
     #plot
-    if do_plot
+    if par.do_plot
         _, psi_separatrix = IMAS.find_psi_boundary(dd)
-        surface, _ = IMAS.flux_surface(eqt,psi_separatrix, :open)
+        surface, _ = IMAS.flux_surface(eqt, psi_separatrix, :open)
         ll = @layout [a{0.6w,0.9h} b{0.4w}]
         p = plot(; layout=ll, size=(1000, 500))
-        plot!(HF, which_plot = :oneD, q = :corerad; subplot =1)
-        plot!(HF, q = :corerad, plot_type =  :path; subplot =2)
+        plot!(HF; which_plot=:oneD, q=:corerad, subplot=1)
+        plot!(HF; q=:corerad, plot_type=:path, subplot=2)
         for surf in surface
-            plot!(surf, color = :grey; subplot =2)
+            plot!(surf; color=:grey, subplot=2)
         end
-        display(plot!())
+        display(p)
     end
     return actor
 end
 
-# Populate dd
 function _finalize(actor::ActorCoreRadHeatFlux)
     # Finalize: work in progress - populate dd
     return actor
--- src/actors/wall_loading/particle_hf_actor.jl
+++ src/actors/wall_loading/particle_hf_actor.jl
@@ -2,19 +2,18 @@
 #  ActorParticleHeatFlux  #
 #= ===================== =#
 
-# Definiton of act parameters
 Base.@kwdef mutable struct FUSEparameters__ActorParticleHeatFlux{T<:Real} <: ParametersActorPlasma{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     _time::Float64 = NaN
+    r::Entry{Vector{T}} = Entry{Vector{T}}("m", "Vector of r at outermidplane"; default=T[])
+    q::Entry{Vector{T}} = Entry{Vector{T}}("W m^-2", "Vector of parallel power density at outer midplane"; default=T[])
+    levels::Entry{Union{Int,Vector{T}}} = Entry{Union{Int,Vector{T}}}("-", "If Int it defines number of levels in SOL, if vector it corresponds to the psi levels to build SOL"; default=20)
+    merge_wall::Entry{Bool} = Entry{Bool}("-", "Merge dd.wall in mesh for the heat flux "; default=true)
+    step::Entry{T} = Entry{T}("m", " Step for discretization of the default wall mesh (dd.wall)"; default=0.1)
     do_plot::Entry{Bool} = act_common_parameters(; do_plot=false)
-    r::Entry{AbstractVector{Float64}} = Entry{AbstractVector{Float64}}("m", "Vector of r at outermidplane"; default = Float64[])
-    q::Entry{AbstractVector{Float64}} = Entry{AbstractVector{Float64}}("W m^-2", "Vector of parallel power density  at outer midplane"; default = Float64[])
-    merge_wall::Entry{Bool} = Entry{Bool}("-","Merge dd.wall in mesh for the heat flux "; default = true)
-    levels::Entry{Union{Int,AbstractVector}} = Entry{Union{Int,AbstractVector}}("-", "If Int it defines number of levels in SOL, if vector it corresponds to the psi levels to build SOL"; default = 20)
-    step::Entry{Real} = Entry{Real}("m"," Step for discretization of the default wall mesh (dd.wall)"; default = 0.1)
 end
-# Definition of Actor struct
+
 mutable struct ActorParticleHeatFlux{D,P} <: SingleAbstractActor{D,P}
     dd::IMAS.dd{D}
     par::FUSEparameters__ActorParticleHeatFlux{P}
@@ -26,12 +25,10 @@ mutable struct ActorParticleHeatFlux{D,P} <: SingleAbstractActor{D,P}
 end
 
 """
-    ActorParticleHeatFlux
+    ActorParticleHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
 
 Computes the heat flux on the wall due to the charged particles
-
 """
-
 function ActorParticleHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
     actor = ActorParticleHeatFlux(dd, act.ActorParticleHeatFlux; kw...)
     step(actor)
@@ -39,18 +36,10 @@ function ActorParticleHeatFlux(dd::IMAS.dd, act::ParametersAllActors; kw...)
     return actor
 end
 
-# Operate actor
 function _step(actor::ActorParticleHeatFlux)
     dd = actor.dd
     par = actor.par
 
-    do_plot::Bool = par.do_plot
-    r::AbstractVector{Float64} = par.r
-    q::AbstractVector{Float64} = par.q
-    merge_wall::Bool = par.merge_wall
-    step::Real = par.step
-    levels::Union{Int,AbstractVector} = par.levels
-   
     eqt = dd.equilibrium.time_slice[]
 
     # Compute wall mesh 
@@ -59,29 +48,29 @@ function _step(actor::ActorParticleHeatFlux)
     # s                     curvilinear abscissa computed from (Rwall, Zwall), clockwise starting at OMP - m
     # SOL                   list of OpenFieldLines used to compute (Rwall, Zwall) 
     # (r,q)                 Hypothesis of power density decay at omp for definition of SOL
-    Rwall, Zwall, rwall, zwall, s, SOL, r, q= IMAS.mesher_HF(dd, r = r, q = q, merge_wall = merge_wall, levels = levels, step = step)
+    Rwall, Zwall, rwall, zwall, s, SOL, r, q = IMAS.mesher_HF(dd; par.r, par.q, par.merge_wall, par.levels, par.step)
 
     # Compute the heat flux due to the influx of charged particles
-    # Qpart                 Heat flux due to particles perpendicular to the wall       - W/m2
-    # Qpara                 Heat flux due to particles parallel to the magnetic field  - W/m2
-    Qpart, Qpara = IMAS.particle_HF(eqt, SOL, rwall, zwall, r, q; merge_wall)
+    # q_part                 Heat flux due to particles perpendicular to the wall       - W/m2
+    # q_parallel             Heat flux due to particles parallel to the magnetic field  - W/m2
+    q_part, q_parallel = IMAS.particle_HF(eqt, SOL, rwall, zwall, r, q; par.merge_wall)
 
-
-    HF = IMAS.WallHeatFlux(r = Rwall, z = Zwall, q_part = Qpart, q_parallel = Qpara, s = s)
+    HF = IMAS.WallHeatFlux(; r=Rwall, z=Zwall, q_part, q_parallel, s)
 
     #plot
-    if do_plot
+    if par.do_plot
         _, psi_separatrix = IMAS.find_psi_boundary(dd)
-        surface, _ = IMAS.flux_surface(eqt,psi_separatrix, :open)
+        surface, _ = IMAS.flux_surface(eqt, psi_separatrix, :open)
         ll = @layout [a{0.6w,0.9h} b{0.4w}]
         p = plot(; layout=ll, size=(1000, 500))
-        plot!(HF, which_plot = :oneD, q = :part; subplot =1)
-        plot!(HF, q = :part, plot_type =  :path; subplot =2)
+        plot!(HF; which_plot=:oneD, q=:part, subplot=1)
+        plot!(HF; q=:part, plot_type=:path, subplot=2)
         for surf in surface
-            plot!(surf, color = :grey; subplot =2)
+            plot!(surf; color=:grey, subplot=2)
         end
-        display(plot!())
+        display(p)
     end
+
     return actor
 end
 
