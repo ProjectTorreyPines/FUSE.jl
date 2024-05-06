3fab6e117130fda9bdc705fce05e1935c3c8db2f
adrianaghiozzi <67669644+adrianaghiozzi@users.noreply.github.com>
2024-05-06 15:59:28 -0700

    Updates to storing data resulting from RABBIT

--- src/actors/hcd/nbi/rabbit_actor.jl
+++ src/actors/hcd/nbi/rabbit_actor.jl
@@ -37,11 +37,12 @@ function _step(actor::ActorRABBIT)
     
     all_inputs = RABBIT.FUSEtoRABBITinput(dd)
 
-    powe_data, powi_data, rho_data, time_data = RABBIT.run_RABBIT(all_inputs; remove_inputs=true)
+    powe_data, powi_data, jnbcd_data, rho_data, time_data = RABBIT.run_RABBIT(all_inputs; remove_inputs=true)
     output = RABBIT.RABBIToutputs()
 
     output.powe_data = powe_data
     output.powi_data = powi_data
+    output.jnbcd_data = jnbcd_data
     output.rho_data = rho_data 
     output.time_data = time_data 
 
@@ -60,15 +61,32 @@ function _finalize(actor::ActorRABBIT)
 
     powe = reshape(actor.outputs.powe_data, num_rho, num_t)
     powi = reshape(actor.outputs.powi_data, num_rho, num_t)
+    jnbcd = reshape(actor.outputs.jnbcd_data, num_rho, num_t)
 
     source = resize!(cs.source, :nbi, "identifier.name" => "nbi"; wipe=false)
-    prof_1d = resize!(source.profiles_1d, num_t)[num_t]
-    ion = resize!(source.profiles_1d[1].ion, 1)[1] # fix this
+    
+    if num_t > 2
+        prof_1d = resize!(source.profiles_1d, num_t)
+    else
+        prof_1d = resize!(source.profiles_1d, 1)
+    end
 
     source.profiles_1d[1].grid.rho_tor_norm = actor.outputs.rho_data
-    for i in 1:length(num_t)
-        source.profiles_1d[i].ion[1].energy = powi[:,i]
-        source.profiles_1d[i].electrons.energy = powe[:,i] 
+    ion = resize!(source.profiles_1d[1].ion, 1)[1]
+
+    source.profiles_1d[1].ion[1].energy = powi[:,1]
+    source.profiles_1d[1].electrons.energy = powe[:,1]
+    source.profiles_1d[1].j_parallel = jnbcd[:,1]
+
+    if num_t > 2
+        for i in 2:length(num_t)
+            source.profiles_1d[i].ion[1].energy = powi[:,i]
+            source.profiles_1d[i].electrons.energy = powe[:,i] 
+        end
+    end
+
+    for nbu in dd.nbi.unit
+        IMAS.ion_element!(ion, "H$(Int(floor(nbu.species.a)))"; fast=true)
     end
 
     p = plot(actor.outputs.rho_data, powe[:,1])
