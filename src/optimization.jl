7c804c0e6274ddba6b50ae2dbe200d4bf8a17825
TimSlendebroek <32385057+TimSlendebroek@users.noreply.github.com>
2024-05-08 13:29:51 -0700

    move ObjectiveFunction to IMAS

--- src/optimization.jl
+++ src/optimization.jl
@@ -4,96 +4,6 @@ import Distributed
 import Dates
 ProgressMeter.ijulia_behavior(:clear)
 
-# ==================== #
-# objectives functions #
-# ==================== #
-mutable struct ObjectiveFunction
-    name::Symbol
-    units::String
-    func::Function
-    target::Float64
-    # inner constructor to register ObjectiveFunction in ObjectiveFunctionsLibrary
-    ObjectiveFunction(name::Symbol, units::String, func::Function, target::Float64) = begin
-        objf = new(name, units, func, target)
-        ObjectiveFunctionsLibrary[objf.name] = objf
-        return objf
-    end
-end
-
-const ObjectiveFunctionsLibrary = Dict{Symbol,ObjectiveFunction}()
-function update_ObjectiveFunctionsLibrary!()
-    empty!(ObjectiveFunctionsLibrary)
-    #! format: off
-    ObjectiveFunction(:min_levelized_CoE, "\$/kWh", dd -> dd.costing.levelized_CoE, -Inf)
-    ObjectiveFunction(:min_log10_levelized_CoE, "log₁₀(\$/kW)", dd -> log10(dd.costing.levelized_CoE), -Inf)
-    ObjectiveFunction(:min_capital_cost, "\$B", dd -> dd.costing.cost_direct_capital.cost / 1E3, -Inf)
-    ObjectiveFunction(:max_fusion, "MW", dd -> IMAS.fusion_power(dd.core_profiles.profiles_1d[]) / 1E6, Inf)
-    ObjectiveFunction(:max_power_electric_net, "MW", dd -> @ddtime(dd.balance_of_plant.power_electric_net) / 1E6, Inf)
-    ObjectiveFunction(:req_power_electric_net, "ΔMW", dd -> abs(@ddtime(dd.balance_of_plant.power_electric_net) - dd.requirements.power_electric_net) / 1E6, 0.0)
-    ObjectiveFunction(:max_flattop, "hours", dd -> dd.build.oh.flattop_duration / 3600.0, Inf)
-    ObjectiveFunction(:max_q95, "", dd -> dd.equilibrium.time_slice[].global_quantities.q_95, Inf)
-    ObjectiveFunction(:req_flattop, "Δhours", dd -> abs(dd.build.oh.flattop_duration - dd.requirements.flattop_duration) / 3600.0, 0.0)
-    ObjectiveFunction(:max_log10_flattop, "log₁₀(hours)", dd -> log10(dd.build.oh.flattop_duration / 3600.0), Inf)
-    ObjectiveFunction(:min_βn, "", dd -> dd.equilibrium.time_slice[].global_quantities.beta_normal, -Inf)
-    ObjectiveFunction(:min_R0, "m", dd -> dd.equilibrium.time_slice[].boundary.geometric_axis.r, -Inf)
-    #! format: on
-    return ObjectiveFunctionsLibrary
-end
-update_ObjectiveFunctionsLibrary!()
-
-"""
-    (objf::ObjectiveFunction)(dd::IMAS.dd)
-
-From real domain to objective domain (Metaheuristics will always minimize)
-"""
-function (objf::ObjectiveFunction)(dd::IMAS.dd)
-    if isinf(objf.target)
-        if objf.target < 0
-            return objf.func(dd)
-        else
-            return -objf.func(dd)
-        end
-    elseif objf.target == 0.0
-        return abs(objf.func(dd))
-    else
-        return abs(objf.func(dd) - objf.target) / objf.target
-    end
-end
-
-"""
-    (objf::ObjectiveFunction)(x::Float64)
-
-From objective domain to real domain
-"""
-function (objf::ObjectiveFunction)(x::Float64)
-    if isinf(objf.target)
-        if objf.target < 0
-            return x
-        else
-            return -x
-        end
-    elseif objf.target == 0.0
-        return x
-    else
-        return x * objf.target + objf.target
-    end
-end
-
-function Base.show(io::IO, f::ObjectiveFunction)
-    printstyled(io, f.name; bold=true, color=:blue)
-    print(io, " →")
-    print(io, " $(f.target)")
-    return print(io, " [$(f.units)]")
-end
-
-function Base.show(io::IO, x::MIME"text/plain", objfs::AbstractDict{Symbol,ObjectiveFunction})
-    for objf in objfs
-        show(io, x, objf)
-        println(io, "")
-    end
-end
-
-
 # =================== #
 # Optimization engine #
 # =================== #
@@ -103,8 +13,8 @@ end
         act::ParametersAllActors,
         actor_or_workflow::Union{Type{<:AbstractActor},Function},
         x::AbstractVector,
-        objective_functions::AbstractVector{<:ObjectiveFunction},
-        constraint_functions::AbstractVector{<:ConstraintFunction},
+        objective_functions::AbstractVector{<:IMAS.ObjectiveFunction},
+        constraint_functions::AbstractVector{<:IMAS.ConstraintFunction},
         save_folder::AbstractString,
         generation::Int,
         save_dd::Bool=true)
@@ -116,8 +26,8 @@ function optimization_engine(
     act::ParametersAllActors,
     actor_or_workflow::Union{Type{<:AbstractActor},Function},
     x::AbstractVector,
-    objective_functions::AbstractVector{<:ObjectiveFunction},
-    constraint_functions::AbstractVector{<: IMAS.ConstraintFunction},
+    objective_functions::AbstractVector{<:IMAS.ObjectiveFunction},
+    constraint_functions::AbstractVector{<:IMAS.ConstraintFunction},
     save_folder::AbstractString,
     generation::Int,
     save_dd::Bool=true)
@@ -203,8 +113,8 @@ function _optimization_engine(
     act::ParametersAllActors,
     actor_or_workflow::Union{Type{<:AbstractActor},Function},
     x::AbstractVector,
-    objective_functions::AbstractVector{<:ObjectiveFunction},
-    constraint_functions::AbstractVector{<: IMAS.ConstraintFunction},
+    objective_functions::AbstractVector{<:IMAS.ObjectiveFunction},
+    constraint_functions::AbstractVector{<:IMAS.ConstraintFunction},
     save_folder::AbstractString,
     generation::Int,
     save_dd::Bool=true)
@@ -221,8 +131,8 @@ end
         act::ParametersAllActors,
         actor_or_workflow::Union{Type{<:AbstractActor},Function},
         X::AbstractMatrix,
-        objective_functions::AbstractVector{<:ObjectiveFunction},
-        constraint_functions::AbstractVector{<:ConstraintFunction},
+        objective_functions::AbstractVector{<:IMAS.ObjectiveFunction},
+        constraint_functions::AbstractVector{<:IMAS.ConstraintFunction},
         save_folder::AbstractString,
         save_dd::Bool,
         p::ProgressMeter.Progress)
@@ -234,7 +144,7 @@ function optimization_engine(
     act::ParametersAllActors,
     actor_or_workflow::Union{Type{<:AbstractActor},Function},
     X::AbstractMatrix,
-    objective_functions::AbstractVector{<:ObjectiveFunction},
+    objective_functions::AbstractVector{<:IMAS.ObjectiveFunction},
     constraint_functions::AbstractVector{<:IMAS.ConstraintFunction},
     save_folder::AbstractString,
     save_dd::Bool,
