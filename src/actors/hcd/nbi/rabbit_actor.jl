1bb8273c272364f9fc88e3fb49bc975d7dee74e2
adrianaghiozzi <67669644+adrianaghiozzi@users.noreply.github.com>
2024-06-12 15:39:55 -0700

    Simplify output

--- src/actors/hcd/nbi/rabbit_actor.jl
+++ src/actors/hcd/nbi/rabbit_actor.jl
@@ -33,24 +33,9 @@ end
 
 function _step(actor::ActorRABBIT)
     dd = actor.dd
-
     all_inputs = RABBIT.FUSEtoRABBITinput(dd)
-
-    powe_data, powi_data, jnbcd_data, bdep_data, torqdepo_data, rho_data, time_data = RABBIT.run_RABBIT(all_inputs; remove_inputs=true)
-    output = RABBIT.RABBIToutput()
-
-    output.powe_data = powe_data
-    output.powi_data = powi_data
-    output.jnbcd_data = jnbcd_data
-    output.bdep_data = bdep_data
-    output.torque_data = torqdepo_data
-    output.rho_data = rho_data
-    output.time_data = time_data
-
-    actor.outputs = output
-
+    actor.outputs = RABBIT.run_RABBIT(all_inputs; remove_inputs = true)
     return actor
-
 end
 
 function _finalize(actor::ActorRABBIT)
@@ -74,7 +59,7 @@ function _finalize(actor::ActorRABBIT)
     source.profiles_1d[1].electrons.energy = output.powe_data[:, 1]
     source.profiles_1d[1].electrons.particles = vec(sum(output.bdep_data[:, 1, :]; dims=2))
     source.profiles_1d[1].j_parallel = output.jnbcd_data[:, 1]
-    source.profiles_1d[1].momentum_tor = vec(sum(output.torque_data[:, 1, :]; dims=2))
+    source.profiles_1d[1].momentum_tor = vec(sum(output.torqdepo_data[:, 1, :]; dims=2))
 
     if num_t > 2
         for i in 2:num_t
@@ -83,7 +68,7 @@ function _finalize(actor::ActorRABBIT)
             source.profiles_1d[i].electrons.energy = output.powe_data[:, i]
             source.profiles_1d[i].electrons.particles = vec(sum(output.bdep_data[:, i, :]; dims=2))
             source.profiles_1d[i].j_parallel = output.jnbcd_data[:, i]
-            source.profiles_1d[i].momentum_tor = vec(sum(output.torque_data[:, i, :]; dims=2))
+            source.profiles_1d[i].momentum_tor = vec(sum(output.torqdepo_data[:, i, :]; dims=2))
         end
     end
 
