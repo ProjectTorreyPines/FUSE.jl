4ce3abe13d458871f08d67c8972fa5fd01c16c2b
Tom Neiser <neisert@fusion.gat.com>
2024-07-17 15:04:28 -0700

    test sat1 em qlnne

--- examples/studies/run_study_TGLFdb.jl
+++ examples/studies/run_study_TGLFdb.jl
@@ -8,17 +8,17 @@ sty,act = FUSE.study_parameters(:TGLFdb);
 sty
 # Interacting with sty
 sty.server = "saga"
-sty.n_workers = 80
+sty.n_workers = 20
 
-sty.database_folder = "/mnt/beegfs/users/neisert/ODSs/d3d"
+sty.database_folder = "/mnt/beegfs/users/neisert/ODSs/mastu_intersect_steady"
 
 #mastu_intersect_steady_hd "#d3d_negDcake_oak"#iri/166066"#d3d"
 
-sty.save_folder = "/mnt/beegfs/users/neisert/ODSs/d3d/outputs_gknn8_rho_0p1_0p05_0p85_rot"
+sty.save_folder = "/mnt/beegfs/users/neisert/ODSs/mastu_intersect_steady/outputs_gknn10_rho_0p1_0p05_0p90_rot"
 
 sty.sat_rules = missing #[:sat1,:sat2,:sat3] #study specific parameters
 # It's also possible to run with a custom tglfnn model, set sty.custom_tglf_models
-sty.custom_tglf_models = ["sat2_em_d3d_azf-1"]
+sty.custom_tglf_models = ["sat1_em_mastu_azf+1"]
 
 # All DIII-D
 #["sat0quench_es_d3d_azf+1", "sat0quench_em_d3d_azf+1", "sat0_es_d3d", "sat0_em_d3d", "sat1_es_d3d", "sat1_em_d3d", "sat2_es_d3d_azf+1", "sat2_es_d3d_azf-1", "sat2_em_d3d_azf+1", "sat2_em_d3d_azf-1", "sat3_es_d3d_azf+1", "sat3_es_d3d_azf-1", "sat3_em_d3d_azf+1", "sat3_em_d3d_azf-1"]
@@ -35,11 +35,11 @@ sty.custom_tglf_models = ["sat2_em_d3d_azf-1"]
 sty.file_save_mode = :overwrite #overwrite or #safe_write
 sty.release_workers_after_run = true # this is the default behavior and releases workers after running the study
 sty
-study = FUSE.StudyTGLFdb(sty, act; n_workers=80); # it is possible to pass in keyword arguments to sty
+study = FUSE.StudyTGLFdb(sty, act; n_workers=20); # it is possible to pass in keyword arguments to sty
 using Distributed
 @everywhere import FUSE
 study.act.ActorFluxMatcher.evolve_rotation = :flux_match
-study.act.ActorFluxMatcher.rho_transport = 0.1:0.05:0.85
+study.act.ActorFluxMatcher.rho_transport = 0.1:0.05:0.90
 study.act.ActorFluxMatcher
 FUSE.run(study);
 FUSE.analyze(study);
