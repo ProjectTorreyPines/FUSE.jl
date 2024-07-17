25cf85c88763f64a52ccc12a01da434e88f5de76
Orso Meneghini <orso82@gmail.com>
2024-07-17 15:12:04 -0700

    update RABBIT actor and notebook

--- src/actors/hcd/nbi/rabbit_actor.jl
+++ src/actors/hcd/nbi/rabbit_actor.jl
@@ -33,8 +33,11 @@ end
 
 function _step(actor::ActorRABBIT)
     dd = actor.dd
-    all_inputs = RABBIT.FUSEtoRABBITinput(dd)
-    actor.outputs = RABBIT.run_RABBIT(all_inputs; remove_inputs = true)
+
+    @assert length(dd.nbi.unit) == 1 "For now only one NBI unit is supported"
+
+    all_inputs = FUSEtoRABBITinput(dd)
+    actor.outputs = RABBIT.run_RABBIT(all_inputs; remove_inputs=true)
     return actor
 end
 
@@ -43,39 +46,163 @@ function _finalize(actor::ActorRABBIT)
     cs = dd.core_sources
     output = actor.outputs
 
-    num_t = length(output.time_data)
-    source = resize!(cs.source, :nbi, "identifier.name" => "nbi"; wipe=true)
+    eqt = dd.equilibrium.time_slice[]
 
-    if num_t > 2
-        prof_1d = resize!(source.profiles_1d, num_t)
-    else
-        prof_1d = resize!(source.profiles_1d, 1)
-    end
+    rho = [0.0; output.rho_data; 1.0]
+    volume = IMAS.interp1d(eqt.profiles_1d.rho_tor_norm, eqt.profiles_1d.volume).(rho)
+    area = IMAS.interp1d(eqt.profiles_1d.rho_tor_norm, eqt.profiles_1d.area).(rho)
 
-    source.profiles_1d[1].grid.rho_tor_norm = output.rho_data
-    ion = resize!(source.profiles_1d[1].ion, 1)[1]
+    for (ps, nbu) in zip(dd.pulse_schedule.nbi.unit, dd.nbi.unit)
+        power_launched = @ddtime(ps.power.reference)
+        beam_energy = @ddtime(nbu.energy.data)
 
-    source.profiles_1d[1].total_ion_energy = output.powi_data[:, 1]
-    source.profiles_1d[1].electrons.energy = output.powe_data[:, 1]
-    source.profiles_1d[1].electrons.particles = vec(sum(output.bdep_data[:, 1, :]; dims=2))
-    source.profiles_1d[1].j_parallel = output.jnbcd_data[:, 1]
-    source.profiles_1d[1].momentum_tor = vec(sum(output.torqdepo_data[:, 1, :]; dims=2))
+        @ddtime(nbu.power_launched.data = power_launched)
 
-    if num_t > 2
-        for i in 2:num_t
-            source.profiles_1d[i].grid.rho_tor_norm = output.rho_data
-            source.profiles_1d[i].total_ion_energy = output.powi_data[:, i]
-            source.profiles_1d[i].electrons.energy = output.powe_data[:, i]
-            source.profiles_1d[i].electrons.particles = vec(sum(output.bdep_data[:, i, :]; dims=2))
-            source.profiles_1d[i].j_parallel = output.jnbcd_data[:, i]
-            source.profiles_1d[i].momentum_tor = vec(sum(output.torqdepo_data[:, i, :]; dims=2))
-        end
-    end
+        # evaluate various source channels
+        # here we also extend the RABBIT grid to the edges
+        # so that the linear interpolation does not extrapolate in weird ways
+        electrons_energy = output.powe_data[:, end]
+        electrons_energy = [electrons_energy[1]; electrons_energy; electrons_energy[end]]
+        total_ion_energy = output.powi_data[:, end]
+        total_ion_energy = [total_ion_energy[1]; total_ion_energy; total_ion_energy[end]]
+        electrons_particles = vec(sum(output.bdep_data[:, end, :]; dims=2))
+        electrons_particles = [electrons_particles[1]; electrons_particles; electrons_particles[end]]
+        j_parallel = output.jnbcd_data[:, end]
+        j_parallel = [j_parallel[1]; j_parallel; j_parallel[end]]
+        momentum_tor = vec(sum(output.torqdepo_data[:, end, :]; dims=2))
+        momentum_tor = [momentum_tor[1]; momentum_tor; momentum_tor[end]]
 
-    for nbu in dd.nbi.unit
+        source = resize!(cs.source, :nbi, "identifier.name" => nbu.name; wipe=false)
+        IMAS.new_source(
+            source,
+            source.identifier.index,
+            nbu.name,
+            rho,
+            volume,
+            area;
+            electrons_energy,
+            total_ion_energy,
+            electrons_particles,
+            j_parallel,
+            momentum_tor
+        )
+
+        # add nbi fast ion particles source
+        ion = resize!(source.profiles_1d[].ion, 1)[1]
         IMAS.ion_element!(ion, "H$(Int(floor(nbu.species.a)))"; fast=true)
+        ion.particles = source.profiles_1d[].electrons.particles
+        ion.particles_inside = source.profiles_1d[].electrons.particles_inside
+        ion.fast_particles_energy = beam_energy
     end
 
     return actor
+end
+
+function FUSEtoRABBITinput(dd::IMAS.dd)
+    eV_to_keV = 1e-3
+    cm3_to_m3 = 1e-6
+
+    eq = dd.equilibrium
+
+    all_inputs = RABBIT.RABBITinput[]
+
+    for eqt in eq.time_slice
+        time = eqt.time
+
+        eqt2d = findfirst(:rectangular, eqt.profiles_2d)
+        if eqt2d === nothing
+            continue
+        end
+
+        inp = RABBIT.RABBITinput()
+        inp.time = time * 1e3
+
+        inp.nw = length(eqt2d.grid.dim1)
+        inp.nh = length(eqt2d.grid.dim2)
+
+        inp.psirz = eqt2d.psi ./ 2pi
+        inp.npsi1d = length(eqt.profiles_1d.psi)
+        inp.qpsi = abs.(eqt.profiles_1d.q) # RABBIT assumes positive q 
+        inp.fpol = eqt.profiles_1d.f
+        inp.sibry = eqt.global_quantities.psi_boundary / 2pi
+        inp.simag = eqt.global_quantities.psi_axis / 2pi
+        inp.signip = sign(eqt.global_quantities.ip)
+        inp.rmaxis = eqt.global_quantities.magnetic_axis.r
+        inp.zmaxis = eqt.global_quantities.magnetic_axis.z
+
+        inp.r = eqt2d.grid.dim1
+        inp.z = eqt2d.grid.dim2
+
+        phi = eqt.profiles_1d.phi
+        rhorz_n = eqt2d.phi ./ last(phi)
+        rhorz_sq = map(x -> x < 0 ? 0 : x, rhorz_n)
+        rhorz = sqrt.(rhorz_sq)
+
+        if inp.simag == inp.sibry
+            psirz_norm = abs.(inp.psirz .- inp.simag)
+        else
+            psirz_norm = abs.(inp.psirz .- inp.simag) ./ (inp.sibry - inp.simag)
+        end
+
+        rhoprz = sqrt.(psirz_norm)
+
+        # as is done in OMFITrabbitEq class, use rho_tor inside lcfs, rho_pol outside (omfit_rabbit.py line 193)
+        rhorz[findall(rhorz .> 1)] .= rhoprz[findall(rhorz .> 1)]
+        inp.rhorz = rhorz
+
+        inp.psi = eqt.profiles_1d.psi ./ 2pi
+        inp.vol = eqt.profiles_1d.volume
+        inp.area = eqt.profiles_1d.area .* 0.0 # this is zeroed out in OMFITrabbitEq class
+
+        cp1d = dd.core_profiles.profiles_1d[time]
+
+        inp.rho = cp1d.grid.rho_tor_norm
+        inp.eq_rho = eqt.profiles_1d.rho_tor_norm
+        inp.n_rho = length(inp.rho)
+
+        inp.te = IMAS.interp1d(cp1d.grid.rho_tor_norm, cp1d.electrons.temperature).(inp.rho) .* eV_to_keV
+        inp.dene = IMAS.interp1d(cp1d.grid.rho_tor_norm, cp1d.electrons.density).(inp.rho) .* cm3_to_m3
+        inp.rot_freq_tor = inp.rho .* 0.0
+        inp.zeff = IMAS.interp1d(cp1d.grid.rho_tor_norm, cp1d.zeff).(inp.rho)
+
+        for i in 2:length(cp1d.ion)
+            @assert cp1d.ion[1].temperature == cp1d.ion[i].temperature "All ion temperatures should be the same"
+        end
+        inp.ti = IMAS.interp1d(cp1d.grid.rho_tor_norm, cp1d.ion[1].temperature).(inp.rho) .* eV_to_keV
+
+        pnbis = Float64[]
+        for ps in dd.pulse_schedule.nbi.unit
+            push!(pnbis, IMAS.get_time_array(ps.power, :reference, time))
+        end
+
+        if length(eq.time_slice) > 2
+            times = ones(length(eq.time_slice) - 1)
+        else
+            times = ones(2)
+        end
+        inp.pnbi = pnbis .* times
+
+        inp.n_sources = length(dd.nbi.unit)
+        inp.injection_energy = dd.nbi.unit[1].energy.data
+        inp.a_beam = [dd.nbi.unit[1].species.a]
+
+        # the settings below reflect the default beams.dat input file for DIII-D from OMFIT
+        inp.nv = 3
+        inp.start_pos = [5.804921, 5.6625959, 0.0000000]
+        inp.beam_unit_vector = [-0.80732277, -0.59011012, 0.0000000]
+        inp.beam_width_polynomial_coefficients = [0.0000000, 0.023835, 0.0000000]
+        inp.particle_fraction = [0.52422392, 0.3088602, 0.16691588]
+
+        push!(all_inputs, inp)
+    end
+
+    if length(all_inputs) == 1
+        inp = deepcopy(all_inputs[1])
+        inp.time = -1e6
+        push!(all_inputs, inp)
+        reverse!(all_inputs)
+    end
+
+    return all_inputs
 
 end
