53c4d0e448b87ed180f6d1b8b168de4a2da96ca4
Orso Meneghini <orso82@gmail.com>
2024-08-16 10:17:43 -0700

    ptp with --get-dir argument

--- docs/src/index.jl
+++ docs/src/index.jl
@@ -17,7 +17,6 @@ FUSE is written completely in Julia, and is structured as follows:
 1. Data is stored in the `dd` data structure, which is based on the ITER IMAS onthology
 1. Physics and engineering `actors` are the fundamental building blocks of FUSE simulations
 1. Actors functionality is controlled via `act` parameters
-1. The data structure can be initialized starting from 0D `ini` parameters
 1. FUSE comes with a series of template `use cases` for different machines (FPP, ITER, ARC, ...)
 1. `workflows` perform self-contained studies/optimizations (typically running many FUSE simulations)
 1. FUSE can interface with the existing GA ecosystem of modeling codes (OMFIT/OMAS) as well as IMAS
