9bde71d9b916f0988321198b73c20b812c356e3d
Orso Meneghini <orso82@gmail.com>
2024-08-16 21:49:18 -0700

    redirect

--- index.jl
+++
@@ -1,9 +0,0 @@
-txt = open(joinpath(@__DIR__, "..", "..", "README.md"), "r") do io
-    return read(io)
-end
-
-
-open(joinpath(@__DIR__, "index.md"), "w") do io
-    write(io, txt)
-    return write(io, "\nLast update on $(Dates.now())")
-end
