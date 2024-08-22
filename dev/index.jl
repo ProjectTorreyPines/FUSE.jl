331a14274e5bf9a5500d4afce15559a4da799823
Documenter.jl <documenter@juliadocs.github.io>
2024-08-22 06:56:16 +0000

    build based on d70c50d

--- dev/index.jl
+++ dev/index.jl
@@ -1,7 +1,10 @@
 txt = open(joinpath(@__DIR__, "..", "..", "README.md"), "r") do io
-    return read(io)
+    return read(io, String)
 end
 
+txt = replace(txt,
+    "## Documentation\nhttps://fuse.help" => "",
+    "![svg](./docs/src/assets/FUSE.svg)" => "![svg](./assets/FUSE.svg)")
 
 open(joinpath(@__DIR__, "index.md"), "w") do io
     write(io, txt)
