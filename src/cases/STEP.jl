f8b2472de6253823af2d2b7860e9461edf2723f3
Orso Meneghini <orso82@gmail.com>
2024-08-29 14:36:32 -0700

    fix STEP case by lowering TEQUILA.relax

--- src/cases/STEP.jl
+++ src/cases/STEP.jl
@@ -213,10 +213,12 @@ function case_parameters(::Type{Val{:STEP_scalars}})::Tuple{ParametersAllInits,P
     act.ActorCoreTransport.model = :FluxMatcher
     act.ActorFluxMatcher.evolve_densities = :flux_match
     act.ActorFluxMatcher.evolve_densities = :fixed
-    act.ActorFluxMatcher.rho_transport=0.3:0.05:0.8
+    act.ActorFluxMatcher.rho_transport = 0.3:0.05:0.8
     act.ActorTGLF.user_specified_model = "sat0_em_d3d"
 
     act.ActorStabilityLimits.models = Symbol[]
+
+    act.ActorTEQUILA.relax = 0.01
 
     set_new_base!(ini)
     set_new_base!(act)
