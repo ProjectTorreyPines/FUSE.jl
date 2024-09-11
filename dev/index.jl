146487d270e4f7c805d393bc52d47f166dbdba24
Documenter.jl <documenter@juliadocs.github.io>
2024-09-11 17:40:48 +0000

    build based on d62c2eb

--- dev/index.jl
+++ dev/index.jl
@@ -4,7 +4,7 @@ end
 
 txt = replace(txt,
     "## Documentation 📚\n\nFind the full documentation here: [https://fuse.help](https://fuse.help)" => "",
-    "![svg](./docs/src/assets/FUSE.svg)" => "![svg](./assets/FUSE.svg)")
+    "docs/src/assets/" => "assets/")
 
 open(joinpath(@__DIR__, "index.md"), "w") do io
     write(io, txt)
