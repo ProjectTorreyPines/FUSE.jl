7f6c575d984b0b93f7f25e23a8e0bddb2cbc2c00
Orso Meneghini <orso82@gmail.com>
2024-10-01 13:48:07 -0700

    bug fix

--- test/runtests_init_expressions.jl
+++ test/runtests_init_expressions.jl
@@ -4,5 +4,5 @@ using Test
 @testset "init_expressions" begin
     all_expr_fields = intersect(FUSE.init_expressions()...)
     all_expr_fields_from_file = FUSE.load_init_expressions()
-    @test isempty(symdiff(all_expr_fields, all_expr_fields_from_file)) "expressions after init() have changed! run `make init_expressions`"
+    @assert isempty(symdiff(all_expr_fields, all_expr_fields_from_file)) "expressions after init() have changed! run `make init_expressions`"
 end
