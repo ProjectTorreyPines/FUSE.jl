526ba2bb66921f8410d3681c8105554146b59058
Documenter.jl <documenter@juliadocs.github.io>
2024-10-12 03:54:16 +0000

    build based on 529155b

---
+++ v0.1.25/deps.jl
@@ -0,0 +1,24 @@
+deps = "ADAS BalanceOfPlantSurrogate BoundaryPlasmaModels CHEASE CoordinateConventions EPEDNN FiniteElementHermite FuseUtils FusionMaterials FuseExchangeProtocol IMAS IMASdd MXHEquilibrium MeshTools MillerExtendedHarmonic NEO NNeutronics QED RABBIT SimulationParameters TEQUILA TGLFNN TJLF VacuumFields XSteam ThermalSystemModels"
+
+txt = String["""
+# Dependencies
+
+The FUSE project is built upon multiple Julia packages, many of which reside in the [https://github.com/ProjectTorreyPines](https://github.com/ProjectTorreyPines) organization on GitHub.
+
+![FUSE dependencies](./assets/deps.svg)
+
+"""]
+for dep in sort!(split(deps))
+    if dep in ("XSteam", "BoundaryPlasmaModels")
+        continue
+        push!(txt, "* $dep [[repo](https://github.com/ProjectTorreyPines/$dep.jl)]")
+    elseif dep in ("ThermalSystemModels",)
+        push!(txt, "* [$dep](https://projecttorreypines.github.io/$dep.jl/) [[repo](https://github.com/ProjectTorreyPines/$dep.jl)]")
+    else
+        push!(txt, "* [$dep](https://projecttorreypines.github.io/$dep.jl/dev/) [[repo](https://github.com/ProjectTorreyPines/$dep.jl)]")
+    end
+end
+
+open("$(@__DIR__)/deps.md", "w") do io
+    return write(io, join(txt, "\n"))
+end
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.1.25",
   "v0.1.24",
   "v0.1.23",
   "v0.1.22",
@@ -14,5 +15,5 @@ var DOC_VERSIONS = [
   "v0.1.10",
   "v0.1.9",
 ];
-var DOCUMENTER_NEWEST = "v0.1.24";
+var DOCUMENTER_NEWEST = "v0.1.25";
 var DOCUMENTER_STABLE = "stable";
