a878a3f4c68af59951747f07c13d8105139d6be6
Orso Meneghini <orso82@gmail.com>
2024-11-08 01:14:08 -0800

    less aggressive m in Anderson

--- src/actors/transport/flux_matcher_actor.jl
+++ src/actors/transport/flux_matcher_actor.jl
@@ -123,7 +123,7 @@ function _step(actor::ActorFluxMatcher)
             if par.optimizer_algorithm == :newton
                 opts = Dict(:method => :newton, :factor => par.step_size)
             elseif par.optimizer_algorithm == :anderson
-                opts = Dict(:method => :anderson, :m => 5, :beta => -par.step_size * 0.5)
+                opts = Dict(:method => :anderson, :m => 3, :beta => -par.step_size * 0.5)
             elseif par.optimizer_algorithm == :trust_region
                 opts = Dict(:method => :trust_region, :factor => par.step_size, :autoscale => true)
             end
