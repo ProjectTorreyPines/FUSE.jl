c26b40aeb449089d711560b62f289251a0fd8db9
Documenter.jl <documenter@juliadocs.github.io>
2024-11-14 23:15:13 +0000

    build based on d0eb220

--- dev/ini_docs.jl
+++ dev/ini_docs.jl
@@ -7,13 +7,14 @@ CurrentModule = FUSE
 
 ```@example
 import FUSE # hide
-ini = FUSE.ParametersInits()
-resize!(ini.build.layers, 1)
-resize!(ini.nb_unit, 1)
-resize!(ini.ec_launcher, 1)
-resize!(ini.pellet_launcher, 1)
-resize!(ini.ic_antenna, 1)
-resize!(ini.lh_antenna, 1)
+ini = FUSE.ParametersInits() # hide
+resize!(ini.build.layers, 1) # hide
+resize!(ini.nb_unit, 1) # hide
+resize!(ini.ec_launcher, 1) # hide
+resize!(ini.pellet_launcher, 1) # hide
+resize!(ini.ic_antenna, 1) # hide
+resize!(ini.lh_antenna, 1) # hide
+ini # hide
 ```
 
 """]
