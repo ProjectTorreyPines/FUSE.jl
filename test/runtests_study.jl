f36be37911a38efd9bcb371a34c9969c15c35bbb
Orso Meneghini <orso82@gmail.com>
2024-11-23 17:42:14 -0800

    somewhat fixed study test, but still fails

--- test/runtests_study.jl
+++ test/runtests_study.jl
@@ -2,22 +2,27 @@ using FUSE
 using Test
 using Distributed
 
-@testset
-    sty,act = FUSE.study_parameters(:DatabaseGenerator)
+@testset "study" begin
+    sty, act = FUSE.study_parameters(:DatabaseGenerator)
     sty.server = "localhost"
     sty.n_workers = 2
     sty.file_save_mode = :append
-    sty.save_folder = mkdtempdir()
+    sty.save_folder = mktempdir()
     sty.n_simulations = 2
-    ini, act = FUSE.case_parameters(:ITER;init_from=:scalars)
+
+    ini, act = FUSE.case_parameters(:ITER; init_from=:scalars)
     ini.core_profiles.ne_setting = :greenwald_fraction
-    ini.core_profiles.ne_value = 0.2 ↔ [0.2,1.0]
-    study = FUSE.StudyDatabaseGenerator(sty,ini, act)
+    ini.core_profiles.ne_value = 0.2 ↔ [0.2, 1.0]
 
     @everywhere import FUSE
+
     @everywhere function workflow_DatabaseGenerator(dd::FUSE.IMAS.dd, ini::FUSE.ParametersAllInits, act::FUSE.ParametersAllActors)
         FUSE.init(dd, ini, act)
         return nothing
     end
+
+    study = FUSE.StudyDatabaseGenerator(sty, ini, act)
+    study.workflow = workflow_DatabaseGenerator
+
     FUSE.run(study)
 end
