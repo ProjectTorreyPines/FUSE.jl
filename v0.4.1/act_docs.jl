62d0422d66fbe990c23f43952400fd993a680351
Documenter.jl <documenter@juliadocs.github.io>
2025-01-17 22:17:03 +0000

    build based on 889df80

---
+++ v0.4.1/act_docs.jl
@@ -0,0 +1,22 @@
+txt = ["""
+# act Parameters
+
+```@meta
+CurrentModule = FUSE
+```
+
+```@example
+import FUSE # hide
+act = FUSE.ParametersActors() # hide
+act # hide
+```
+
+"""]
+
+open("$(@__DIR__)/act.md", "w") do io
+    return write(io, join(txt, "\n"))
+end
+
+open("$(@__DIR__)/act_details.md", "w") do io
+    return parameters_details_md(io, FUSE.ParametersActors())
+end
