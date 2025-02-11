cbfaacb7d9e98a37f0d33c6d7a3b7a7df916b9d9
Orso Meneghini <orso82@gmail.com>
2025-02-11 09:40:19 -0800

    replay for ActorCoreTransport

--- src/actors/transport/core_transport_actor.jl
+++ src/actors/transport/core_transport_actor.jl
@@ -5,7 +5,7 @@ Base.@kwdef mutable struct FUSEparameters__ActorCoreTransport{T<:Real} <: Parame
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     _time::Float64 = NaN
-    model::Switch{Symbol} = Switch{Symbol}([:FluxMatcher, :EPEDProfiles, :none], "-", "Transport actor to run"; default=:FluxMatcher)
+    model::Switch{Symbol} = Switch{Symbol}([:FluxMatcher, :EPEDProfiles, :replay, :none], "-", "Transport actor to run"; default=:FluxMatcher)
     do_plot::Entry{Bool} = act_common_parameters(do_plot=false)
 end
 
@@ -13,7 +13,7 @@ mutable struct ActorCoreTransport{D,P} <: CompoundAbstractActor{D,P}
     dd::IMAS.dd{D}
     par::FUSEparameters__ActorCoreTransport{P}
     act::ParametersAllActors{P}
-    tr_actor::Union{ActorFluxMatcher{D,P},ActorEPEDprofiles{D,P},ActorNoOperation{D,P}}
+    tr_actor::Union{ActorFluxMatcher{D,P},ActorEPEDprofiles{D,P},ActorReplay{D,P},ActorNoOperation{D,P}}
 end
 
 """
@@ -31,14 +31,19 @@ end
 function ActorCoreTransport(dd::IMAS.dd, par::FUSEparameters__ActorCoreTransport, act::ParametersAllActors; kw...)
     logging_actor_init(ActorCoreTransport)
     par = par(kw...)
+    
+    noop = ActorNoOperation(dd, act.ActorNoOperation)
+    actor = ActorCoreTransport(dd, par, act, noop)
+    
     if par.model == :FluxMatcher
-        tr_actor = ActorFluxMatcher(dd, act.ActorFluxMatcher, act; par.do_plot)
+        actor.tr_actor = ActorFluxMatcher(dd, act.ActorFluxMatcher, act; par.do_plot)
     elseif par.model == :EPEDProfiles
-        tr_actor = ActorEPEDprofiles(dd, act.ActorEPEDprofiles)
-    elseif par.model == :none
-        tr_actor = ActorNoOperation(dd, act.ActorNoOperation)
+        actor.tr_actor = ActorEPEDprofiles(dd, act.ActorEPEDprofiles)
+    elseif par.model == :replay
+        actor.tr_actor = ActorReplay(dd, act.ActorReplay, actor)
     end
-    return ActorCoreTransport(dd, par, act, tr_actor)
+    
+    return actor
 end
 
 """
@@ -59,4 +64,9 @@ Finalizes the selected core transport actor finalize
 function _finalize(actor::ActorCoreTransport)
     finalize(actor.tr_actor)
     return actor
+end
+
+function _step(replay_actor::ActorReplay, actor::ActorCoreTransport, replay_dd::IMAS.dd)
+    IMAS.IMASdd.copy_timeslice!(actor.dd.core_profiles, replay_dd.core_profiles, actor.dd.global_time);
+    return replay_actor
 end
