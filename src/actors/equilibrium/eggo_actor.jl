7d1dde8aedb084c0b67d8d21f2b2e2e19d4070fe
Orso Meneghini <orso82@gmail.com>
2025-02-27 10:04:22 -0800

    update ActorEGGO

--- src/actors/equilibrium/eggo_actor.jl
+++ src/actors/equilibrium/eggo_actor.jl
@@ -20,6 +20,7 @@ mutable struct ActorEGGO{D,P} <: CompoundAbstractActor{D,P}
     act::ParametersAllActors{P}
     green::Dict{Symbol,Any}
     basis_functions::Dict{Symbol,Any}
+    basis_functions_1d::Dict{Any,Any}
     NNmodel::Dict{Any,Any}
 end
 
@@ -43,8 +44,9 @@ function ActorEGGO(dd::IMAS.dd{D}, par::FUSEparameters__ActorEGGO{P}, act::Param
     green = EGGO.get_greens_function_tables(model_name)
     basis_functions = EGGO.get_basis_functions(model_name, green)
     NNmodel = EGGO.get_model(model_name)
+    basis_functions_1d, _ = EGGO.get_basis_functions_1d(model_name)
 
-    return ActorEGGO(dd, par, act, green, basis_functions, NNmodel)
+    return ActorEGGO(dd, par, act, green, basis_functions, basis_functions_1d, NNmodel)
 end
 
 """
@@ -58,33 +60,23 @@ function _step(actor::ActorEGGO{D,P}) where {D<:Real,P<:Real}
     eqt = dd.equilibrium.time_slice[]
     eqt1d = eqt.profiles_1d
 
-    nb = 101
-    nw = actor.green[:nw]
-
     wall = Dict{Symbol,Vector{Float64}}()
     wall[:rlim], wall[:zlim] = IMAS.first_wall(dd.wall)
 
-    Rb_target, Zb_target = IMAS.resample_2d_path(eqt.boundary.outline.r, eqt.boundary.outline.z; n_points=nb)
-    IMAS.reorder_flux_surface!(Rb_target, Zb_target, argmin(Rb_target))
+    Rb_target, Zb_target = eqt.boundary.outline.r, eqt.boundary.outline.z
     b0 = eqt.global_quantities.vacuum_toroidal_field.b0
     r0 = eqt.global_quantities.vacuum_toroidal_field.r0
-    Rcenter = 1.6955
-    Btcenter = r0 * b0 / Rcenter
-    pend = 0.1
+    pend = eqt.profiles_1d.pressure[end]
 
-    psi_norm = range(0.0, 1.0, nw)
-    if true # EFIT01
-        pp_target = range(eqt1d.dpressure_dpsi[1], eqt1d.dpressure_dpsi[end], nw) * 2π
-        ffp_target = range(eqt1d.f_df_dpsi[1], eqt1d.f_df_dpsi[end], nw) * 2π
-    else
-        pp_target = IMAS.interp1d(eqt1d.psi_norm, eqt1d.dpressure_dpsi).(psi_norm) * 2π
-        ffp_target = IMAS.interp1d(eqt1d.psi_norm, eqt1d.f_df_dpsi).(psi_norm) * 2π
-    end
+    psi_norm = collect(range(0.0, 1.0, actor.green[:nw]))
+    pp_target = IMAS.interp1d(eqt1d.psi_norm, eqt1d.dpressure_dpsi).(psi_norm) * 2π
+    ffp_target = IMAS.interp1d(eqt1d.psi_norm, eqt1d.f_df_dpsi).(psi_norm) * 2π
     ecurrt_target = fill(0.0, 6)
 
-    Jt, psirz, Ip, fcurrt = EGGO.predict_model(Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, actor.NNmodel, actor.green, actor.basis_functions)
+    Jt, psirz, Ip, fcurrt =
+        EGGO.predict_model(Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, actor.NNmodel, actor.green, actor.basis_functions, actor.basis_functions_1d)
 
-    EGGO.get_surfaces(eqt, Matrix(transpose(psirz)), Ip, fcurrt, actor.green, wall, Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, Btcenter, Rcenter, pend)
+    EGGO.get_surfaces(eqt, Matrix(transpose(psirz)), Ip, fcurrt, actor.green, wall, Rb_target, Zb_target, pp_target, ffp_target, ecurrt_target, b0, r0, pend)
 
     return actor
 end
