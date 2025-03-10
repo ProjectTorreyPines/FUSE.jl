dab5c5e5da81c63df439049e1a5217d258089f4d
Orso Meneghini <meneghini@fusion.gat.com>
2025-03-09 22:21:54 -0700

    setup EC details to work with torbeam

--- src/actors/hcd/hcd_actor.jl
+++ src/actors/hcd/hcd_actor.jl
@@ -43,6 +43,13 @@ function ActorHCD(dd::IMAS.dd, par::FUSEparameters__ActorHCD, act::ParametersAll
     actor = ActorHCD(dd, par, act, noop, noop, noop, noop, noop)
 
     @assert length(dd.pulse_schedule.ec.beam) == length(dd.ec_launchers.beam) "length(dd.pulse_schedule.ec.beam)=$(length(dd.pulse_schedule.ec.beam)) VS length(dd.ec_launchers.beam)=$(length(dd.ec_launchers.beam))"
+    # fill missing EC launcher hardware details
+    if par.ec_model in (:ECsimple, :TORBEAM)
+        eqt = dd.equilibrium.time_slice[]
+        for (k, ecb) in enumerate(dd.ec_launchers.beam)
+            setup(ecb, eqt, dd.wall, act.ActorSimpleEC.actuator[k])
+        end
+    end
     if par.ec_model == :ECsimple
         actor.ec_actor = ActorSimpleEC(dd, act.ActorSimpleEC)
     elseif par.ec_model == :TORBEAM
@@ -118,6 +125,47 @@ function _finalize(actor::ActorHCD)
     finalize(actor.pellet_actor)
 
     return actor
+end
+
+function setup(ecb::IMAS.ec_launchers__beam, eqt::IMAS.equilibrium__time_slice, wall::IMAS.wall, par::_FUSEparameters__ActorSimpleECactuator)
+    # Estimate operating frequency and mode
+    if ismissing(ecb.frequency, :data)
+        resonance = IMAS.ech_resonance(eqt)
+        ecb.frequency.time = [-Inf]
+        ecb.frequency.data = [resonance.frequency]
+        ecb.mode = resonance.mode == "X" ? -1 : 1
+    end
+    # Pick a reasonable launch location
+    if ismissing(ecb.launching_position, :r) || ismissing(ecb.launching_position, :z)
+        fw = IMAS.first_wall(wall)
+        index = argmax(fw.r .+ fw.z)
+        @ddtime(ecb.launching_position.r = fw.r[index])
+        @ddtime(ecb.launching_position.z = fw.z[index])
+    end
+    if ismissing(ecb.launching_position, :phi)
+        @ddtime(ecb.launching_position.phi = 0.0)
+    end
+    # beam properties
+    if ismissing(ecb.phase, :angle) || ismissing(ecb.phase, :curvature)
+        @ddtime(ecb.phase.angle = 0.0)
+        @ddtime(ecb.phase.curvature = [0.0; 0.0;;])
+    end
+    if ismissing(ecb.spot, :angle) || ismissing(ecb.spot, :size)
+        @ddtime(ecb.spot.angle = 0.0)
+        @ddtime(ecb.spot.size = [0.0172; 0.0172;;])
+    end
+    # aiming based on rho0
+    if (ismissing(ecb, :steering_angle_tor) || ismissing(ecb, :steering_angle_pol)) && !ismissing(par, :rho_0)
+        launch_r = @ddtime(ecb.launching_position.r)
+        launch_z = @ddtime(ecb.launching_position.z)
+        resonance_layer = IMAS.ech_resonance_layer(eqt, IMAS.frequency(ecb))
+        _, _, RHO_interpolant = IMAS.ρ_interpolant(eqt)
+        rho_resonance_layer = RHO_interpolant.(resonance_layer.r, resonance_layer.z)
+        index = resonance_layer.z .> eqt.global_quantities.magnetic_axis.z
+        sub_index = argmin(abs.(rho_resonance_layer[index] .- par.rho_0))
+        @ddtime(ecb.steering_angle_tor = 0.0)
+        @ddtime(ecb.steering_angle_pol = atan(resonance_layer.z[index][sub_index] - launch_z, resonance_layer.r[index][sub_index] - launch_r) + pi)
+    end
 end
 
 function _step(replay_actor::ActorReplay, actor::ActorSimpleEC, replay_dd::IMAS.dd)
