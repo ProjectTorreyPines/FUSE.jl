3af9aa37880a8375fa60f4c181f09c4bf12ebae6
Orso Meneghini <orso82@gmail.com>
2025-03-25 17:07:35 -0700

    adjust QED target current to account metrics integration error at the edge

--- src/actors/current/qed_actor.jl
+++ src/actors/current/qed_actor.jl
@@ -72,6 +72,11 @@ function _step(actor::ActorQED)
         actor.QO.JBni = JBni
     end
 
+    # QED calculates the total current based on q, which goes to infinity at the separatrix
+    # this leads to some small but not negligible difference in the total current calculated
+    # internally by QED and the one from `dd`.
+    ratio = QED.Ip(actor.QO) / IMAS.Ip(cp1d, eqt)
+
     if par.Nt == 0
         # only initialize, nothing to do
 
@@ -93,7 +98,7 @@ function _step(actor::ActorQED)
 
         for time0 in range(t0, t1, No + 1)[1:end-1]
             if par.solve_for == :ip
-                Ip = IMAS.get_from(dd, Val{:ip}, par.ip_from; time0)
+                Ip = IMAS.get_from(dd, Val{:ip}, par.ip_from; time0) * ratio
                 Vedge = nothing
                 if par.allow_floating_plasma_current && abs(Ip) < abs(ip_non_inductive)
                     Ip = nothing
@@ -101,7 +106,7 @@ function _step(actor::ActorQED)
                 end
             else
                 Ip = nothing
-                Vedge = IMAS.get_from(dd, Val{:vloop}, par.vloop_from; time0)
+                Vedge = IMAS.get_from(dd, Val{:vloop}, par.vloop_from; time0) * ratio
             end
 
             # check where q<1 based on the the q-profile at the previous
@@ -118,7 +123,7 @@ function _step(actor::ActorQED)
     elseif par.Δt == Inf
         # steady state solution
         if par.solve_for == :ip
-            Ip = IMAS.get_from(dd, Val{:ip}, par.ip_from)
+            Ip = IMAS.get_from(dd, Val{:ip}, par.ip_from) * ratio
             Vedge = nothing
             if par.allow_floating_plasma_current && abs(Ip) < abs(ip_non_inductive)
                 Ip = nothing
@@ -126,7 +131,7 @@ function _step(actor::ActorQED)
             end
         else
             Ip = nothing
-            Vedge = IMAS.get_from(dd, Val{:vloop}, par.vloop_from)
+            Vedge = IMAS.get_from(dd, Val{:vloop}, par.vloop_from) * ratio
         end
 
         # we need to run steady state twice, the first time to find the q-profile when the
