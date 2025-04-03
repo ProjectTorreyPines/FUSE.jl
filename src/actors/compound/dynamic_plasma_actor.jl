f0382a2e6ea6de6eb5fe245150e17cd0960e3424
Orso Meneghini <orso82@gmail.com>
2025-04-03 13:17:04 -0700

    option to aggregate_hcd in plot_plasma_overview

--- src/actors/compound/dynamic_plasma_actor.jl
+++ src/actors/compound/dynamic_plasma_actor.jl
@@ -300,6 +300,7 @@ Inclusinon in BEAMER presentation can then be done with:
 function plot_plasma_overview(dd::IMAS.dd{T}, time0::Float64=dd.global_time;
     min_power::Float64=0.0,
     aggregate_radiation::Bool=true,
+    aggregate_hcd::Bool=true,
     dd1::Union{Nothing,IMAS.DD}=nothing,
     kw...) where {T<:Real}
 
@@ -460,6 +461,7 @@ function plot_plasma_overview(dd::IMAS.dd{T}, time0::Float64=dd.global_time;
     #     subplot,
     #     min_power,
     #     aggregate_radiation,
+    #     aggregate_hcd,
     #     legend=:topleft,
     #     legend_foreground_color=:transparent,
     #     title="Parallel current",
@@ -475,6 +477,7 @@ function plot_plasma_overview(dd::IMAS.dd{T}, time0::Float64=dd.global_time;
         subplot,
         min_power,
         aggregate_radiation,
+        aggregate_hcd,
         legend=:topleft,
         legend_foreground_color=:transparent,
         title="Electron power source",
@@ -490,6 +493,7 @@ function plot_plasma_overview(dd::IMAS.dd{T}, time0::Float64=dd.global_time;
         subplot,
         min_power,
         aggregate_radiation,
+        aggregate_hcd,
         legend=:topleft,
         legend_foreground_color=:transparent,
         title="Ion power source",
@@ -505,6 +509,7 @@ function plot_plasma_overview(dd::IMAS.dd{T}, time0::Float64=dd.global_time;
         subplot,
         min_power,
         aggregate_radiation,
+        aggregate_hcd,
         legend=:topleft,
         legend_foreground_color=:transparent,
         title="Particle source",
