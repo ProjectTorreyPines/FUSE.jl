03b1195a10779a443f418f81c54df0190b781633
Orso Meneghini <orso82@gmail.com>
2025-04-22 14:34:50 -0700

    fix angles
    mention https://github.com/ProjectTorreyPines/IMAS.jl/pull/253

--- src/actors/hcd/ec/ec_simple_actor.jl
+++ src/actors/hcd/ec/ec_simple_actor.jl
@@ -79,6 +79,11 @@ function _step(actor::ActorSimpleEC)
             t_intersect = 1.0
             power_launched = 0.0
         end
+        # x, y, z, r = IMAS.pencil_beam([launch_r, 0.0, launch_z], angle_pol, angle_tor, range(0.0, 10.0, 100))
+        # plot(eqt;cx=true,coordinate=:rho_tor_norm)
+        # plot!(resonance_layer.r, resonance_layer.z)
+        # plot!(r,z)
+        # display(plot!())
         x, y, z, r = IMAS.pencil_beam([launch_r, 0.0, launch_z], angle_pol, angle_tor, range(0.0, t_intersect, 100))
         rho_0 = RHO_interpolant.(r[end], z[end])
 
@@ -169,6 +174,6 @@ function setup(ecb::IMAS.ec_launchers__beam, eqt::IMAS.equilibrium__time_slice, 
         index = resonance_layer.z .> eqt.global_quantities.magnetic_axis.z
         sub_index = argmin_abs(rho_resonance_layer[index], par.rho_0)
         @ddtime(ecb.steering_angle_tor = 0.0)
-        @ddtime(ecb.steering_angle_pol = atan(resonance_layer.r[index][sub_index] - launch_r, resonance_layer.z[index][sub_index] - launch_z) + pi / 2)
+        @ddtime(ecb.steering_angle_pol = atan((resonance_layer.z[index][sub_index] - launch_z) / (resonance_layer.r[index][sub_index] - launch_r)))
     end
 end
