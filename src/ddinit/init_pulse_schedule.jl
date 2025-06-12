f8ca7be4f7359029e4acefbb0f2352d893c80478
Orso Meneghini <orso82@gmail.com>
2025-06-12 12:39:29 -0700

    wall_radii pulse_schedule can use dd.wall

--- src/ddinit/init_pulse_schedule.jl
+++ src/ddinit/init_pulse_schedule.jl
@@ -80,7 +80,11 @@ function init_pulse_schedule!(dd::IMAS.dd, ini::ParametersAllInits, act::Paramet
                 if ismissing(ini.rampup, :ends_at)
                     init_pulse_schedule_postion_control(ps.position_control, mxhb, time0)
                 else
-                    wr = wall_radii(mxhb.mxh.R0, mxhb.mxh.minor_radius, ini.build.plasma_gap)
+                    if isempty(dd.wall)
+                        wr = wall_radii(mxhb.mxh.R0, mxhb.mxh.minor_radius, ini.build.plasma_gap)
+                    else
+                        wr = wall_radii(IMAS.first_wall(dd.wall)..., mxhb.mxh.Z0)
+                    end
                     mxh_bore, mxh_lim2div = limited_to_diverted(0.75, mxhb, wr.r_hfs, wr.r_lfs, ini.rampup.side)
                     if time0 <= 0.0
                         init_pulse_schedule_postion_control(ps.position_control, mxh_bore, time0)
