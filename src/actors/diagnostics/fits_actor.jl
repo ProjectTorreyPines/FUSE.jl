468e890a830b77947e695aa2143125afc1d5beec
Orso Meneghini <orso82@gmail.com>
2025-08-12 12:26:17 -0700

    improved interferometer verbosity

--- src/actors/diagnostics/fits_actor.jl
+++ src/actors/diagnostics/fits_actor.jl
@@ -138,13 +138,13 @@ function _step(actor::ActorFitProfiles{D,P}) where {D<:Real,P<:Real}
         end
 
         # optimization scales density for groups of thomson scattering channels
-        function cost(scale)
-            scale = abs.(scale)
+        function cost(scales)
+            scales = abs.(scales)
             data = deepcopy(nes)
             c = Float64[]
             for (kt, time0) in enumerate(interferometer_calibration_times)
                 for (kch, subsystem) in enumerate(keys(ts_subsystems_mapper))
-                    data[kt][ts_subsystems_mapper[subsystem]] .*= scale[kch]
+                    data[kt][ts_subsystems_mapper[subsystem]] .*= scales[kch]
                 end
                 eqt = dd.equilibrium.time_slice[time0]
                 index = sortperm(chρs[kt])
@@ -159,11 +159,12 @@ function _step(actor::ActorFitProfiles{D,P}) where {D<:Real,P<:Real}
         res = Optim.optimize(cost, fill(1.0, length(ts_subsystems_mapper)), Optim.NelderMead())
 
         # scale raw data in thomson_scattering IDS
-        scale = res.minimizer
-        @info "Thomson subsystems scaled by interferometer: $scale"
+        scales = res.minimizer
+        scales_string = join(["$subsystem_name=$(@sprintf("%3.3f",scale))" for (subsystem_name,scale) in zip(keys(ts_subsystems_mapper),scales)], ", ")
+        @info "Thomson subsystems scaled to match interferometer measurements: $scales_string"
         for (kch, subsystem) in enumerate(keys(ts_subsystems_mapper))
             for chnum in ts_subsystems_mapper[subsystem]
-                dd.thomson_scattering.channel[chnum].n_e.data .*= scale[kch]
+                dd.thomson_scattering.channel[chnum].n_e.data .*= scales[kch]
             end
         end
     end
