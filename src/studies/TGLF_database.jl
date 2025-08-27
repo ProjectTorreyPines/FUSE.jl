45ba1cc97ecd02145a06ae3be84272dae0c3875c
Orso Meneghini <orso82@gmail.com>
2025-08-27 14:19:01 -0700

    recipie to plot StudyTGLFdb
    mention @tomneiser

--- src/studies/TGLF_database.jl
+++ src/studies/TGLF_database.jl
@@ -282,42 +282,75 @@ function preparse_input(database_folder)
     end
 end
 
-function plot_xy_wth_hist2d(study::StudyTGLFdb; quantity::Symbol=:WTH, save_fig::Bool=false, save_path::AbstarctString="")
+@recipe function plot_xy_wth_hist2d(study::StudyTGLFdb; quantity=:WTH, item_index=nothing)
     if study.act.ActorTGLF.electromagnetic
         EM_contribution = :EM
     else
         EM_contribution = :ES
     end
 
-    for item in study.iterator
-        plot_xy_wth_hist2d(study.dataframes_dict[string(item)], string(item), EM_contribution, quantity, save_fig, save_path)
-    end
-end
-
-function plot_xy_wth_hist2d(df::DataFrame, name::String, EM_contribution::Symbol, quantity::Symbol, save_fig::Bool, save_path::AbstarctString)
-    x = df[!, "$(quantity)_exp"]
-    y = df[!, "$(quantity)"]
-
-    MRE = round(100 * mean_relative_error(x, y); digits=2)
-
-    bins = 10 .^ (4:0.05:7)
-
-    ticks = ([10^4, 10^5, 10^6, 10^7, 10^8], [L"10^4", L"10^5", L"10^6", L"10^7", L"10^8"])
-    xy_lim = [bins[1], bins[end]]
-
-    p = histogram2d(x, y; xscale=:log10, yscale=:log10, bins=(bins, bins), xticks=ticks, yticks=ticks,
-        color=cgrad(:magma; rev=true), colorbar=true, show_empty_bins=true,
-        ylabel="Thermal stored energy predicted [J]", xlabel="Thermal stored energy experiment [J]",
-        ylim=xy_lim, xlim=xy_lim, tickfont=font(12, "Computer Modern"), fontfamily="Computer Modern",
-        xguidefontsize=15, yguidefontsize=14)
-
-    plot!(xy_lim, xy_lim; linestyle=:dash, color=:black, label=nothing, title="SAT$name $EM_contribution")
-
-    println("MRE SAT$name $(EM_contribution) W_thermal = $MRE % with N = $(length(x))")
-
-    if save_fig
-        savefig(p, save_path)
+    # If item_index is specified, plot only that item, otherwise plot all
+    items_to_plot = if item_index === nothing
+        collect(1:length(study.iterator))
     else
-        display(p)
+        [item_index]
     end
+
+    for (i, idx) in enumerate(items_to_plot)
+        item = study.iterator[idx]
+        df = study.dataframes_dict[string(item)]
+        
+        # Check that quantity is supported based on DataFrame columns
+        exp_col = "$(quantity)_exp"
+        pred_col = "$(quantity)"
+        @assert exp_col in names(df) "Quantity $(quantity) not supported: column '$exp_col' not found in DataFrame. Available columns: $(names(df))"
+        @assert pred_col in names(df) "Quantity $(quantity) not supported: column '$pred_col' not found in DataFrame. Available columns: $(names(df))"
+        
+        x = df[!, exp_col]
+        y = df[!, pred_col]
+
+        MRE = round(100 * sum(abs, (y .- x) ./ x) / length(x); digits=2)
+
+        bins = 10 .^ (4:0.05:7)
+        ticks_vals = [10^4, 10^5, 10^6, 10^7, 10^8]
+        ticks_labels = [L"10^4", L"10^5", L"10^6", L"10^7", L"10^8"]
+        xy_lim = [bins[1], bins[end]]
+
+        @series begin
+            seriestype := :histogram2d
+            xscale := :log10
+            yscale := :log10
+            bins := (bins, bins)
+            xticks := (ticks_vals, ticks_labels)
+            yticks := (ticks_vals, ticks_labels)
+            color := cgrad(:magma; rev=true)
+            colorbar := true
+            show_empty_bins := true
+            ylabel := "Thermal stored energy predicted [J]"
+            xlabel := "Thermal stored energy experiment [J]"
+            ylims := xy_lim
+            xlims := xy_lim
+            tickfontsize := 12
+            tickfontfamily := "Computer Modern"
+            guidefontfamily := "Computer Modern"
+            xguidefontsize := 15
+            yguidefontsize := 14
+            title := "SAT$item $EM_contribution"
+            subplot := i
+            x, y
+        end
+
+        @series begin
+            seriestype := :line
+            linestyle := :dash
+            color := :black
+            label := ""
+            subplot := i
+            xy_lim, xy_lim
+        end
+
+        @info("MRE SAT$item $(EM_contribution) W_thermal = $MRE % with N = $(length(x))")
+    end
+
+    layout := length(items_to_plot) > 1 ? length(items_to_plot) : 1
 end
