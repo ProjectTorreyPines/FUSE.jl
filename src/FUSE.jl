758d55cf90c7af82c489c6aa47ee7084da1ba61c
Min-Gu Yoo <mgyoo86@gmail.com>
2025-09-06 11:34:32 -0700

    rename: `study_database.jl`

--- src/FUSE.jl
+++ src/FUSE.jl
@@ -178,7 +178,7 @@ include("studies.jl")
 include(joinpath("studies", "database_generator.jl"))
 include(joinpath("studies", "multi_objective_optimization.jl"))
 include(joinpath("studies", "TGLF_database.jl"))
-include(joinpath("studies", "database_io.jl"))
+include(joinpath("studies", "study_database.jl"))
 
 #= ========= =#
 #  WORKFLOWS  #
