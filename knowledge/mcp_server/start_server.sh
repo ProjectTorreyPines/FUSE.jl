a58aabce9363b3677ad5a4294ddf61c6f2ebbe7f
Tim Slendebroek <tim@slendebroek.com>
2025-09-10 17:06:24 -0700

    improve README and start mcp server

--- knowledge/mcp_server/start_server.sh
+++ knowledge/mcp_server/start_server.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 # Start FUSE MCP Server with proper environment
+echo "change this file to get it to work:: $(realpath "${BASH_SOURCE[0]}")"
 
 # Activate conda environment
-source ~/miniconda3/bin/activate
-conda activate fuse_mcp
+source /Users/tims/.zshrc
+conda activate mcp_server # this is your conda env
 
 # Change to server directory
-cd "/Users/meneghini/.julia/dev/FUSE/knowledge/mcp_server"
+cd "/Users/tims/.julia/dev/FUSE/knowledge/mcp_server/"
 
 # Start the MCP server
 exec python server.py
-
