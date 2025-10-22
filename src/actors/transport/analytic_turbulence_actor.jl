dfb78d4799eb4b22e84207d5d5c5cec586d914aa
jmcclena <jmcclena@gmail.com>
2025-10-22 14:03:50 -0700

    Update src/actors/transport/analytic_turbulence_actor.jl
    Co-authored-by: Brendan Lyons <lyonsbc@fusion.gat.com>

--- src/actors/transport/analytic_turbulence_actor.jl
+++ src/actors/transport/analytic_turbulence_actor.jl
@@ -117,7 +117,7 @@ function _step(actor::ActorAnalyticTurbulence{D,P}) where {D<:Real,P<:Real}
         Dp = @. (A1 + (A2 - A1) * rho_cp) * χe * χi / (χe + χi)
         vin = @. 0.5 * Dp * (surf ^ 2 / volume) / vprime_miller
         vin[1] = 0.0
-        Γe = @. Dp * dlnnedr * ne .- vin * ne
+        Γe = @. (Dp * dlnnedr - vin) * ne
 
         gridpoint_cp = [argmin_abs(cp1d.grid.rho_tor_norm, ρ) for ρ in par.rho_transport]
 
