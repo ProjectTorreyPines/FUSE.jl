696c24e26d24592c107b36f906be442bf4ef4edf
Nan Shi <shinan@fusion.gat.com>
2025-10-23 13:04:47 -0700

    Update divertors_actor.jl
    fix(divertor): ensure SOL boundary results are written back via populate_divertor_targets_from_sol!

--- src/actors/divertors/divertors_actor.jl
+++ src/actors/divertors/divertors_actor.jl
@@ -3,14 +3,10 @@
 #= ============== =#
 import BoundaryPlasmaModels
 
-<<<<<<< Updated upstream
-@actor_parameters_struct ActorDivertors{T} begin
-=======
 Base.@kwdef mutable struct FUSEparameters__ActorDivertors{T<:Real} <: ParametersActor{T}
     _parent::WeakRef = WeakRef(nothing)
     _name::Symbol = :not_set
     _time::Float64 = NaN
->>>>>>> Stashed changes
     boundary_model::Switch{Symbol} = Switch{Symbol}([:slcoupled, :lengyel, :stangeby], "-", "SOL boundary model"; default=:slcoupled)
     impurities::Entry{Vector{Symbol}} = Entry{Vector{Symbol}}("-", "Vector of impurity species"; default=Symbol[])
     impurities_fraction::Entry{Vector{T}} = Entry{Vector{T}}("-", "Vector of impurity fractions"; default=T[])
@@ -68,84 +64,13 @@ function _step(actor::ActorDivertors)
         index_outer = 1
     end
 
-<<<<<<< Updated upstream
-    for strike_index in 1:2
-=======
     strike_indices = (index_outer, index_inner)
     empty!(actor.boundary_plasma_models)
     for (i, strike_index) in enumerate(strike_indices)
->>>>>>> Stashed changes
         boundary_plasma_model = BoundaryPlasmaModels.SOLBoundaryModel(par.boundary_model)
         push!(actor.boundary_plasma_models, boundary_plasma_model)
         BoundaryPlasmaModels.setup_model(boundary_plasma_model, λ_omp, eqt, cp1d, cs, sol1; par.impurities, par.impurities_fraction, par.heat_spread_factor,strike_index)
         boundary_plasma_model() # run
-<<<<<<< Updated upstream
-    end
-
-    # identifiers = IMAS.identify_strike_surface(sol1, dd.divertors)
-    # identifiers = [(k_divertor, k_target) for (k_divertor, k_target, k_tile) in identifiers]
-
-    # for (k_divertor, divertor) in enumerate(dd.divertors.divertor)
-    #     for (k_target, target) in enumerate(divertor.target)
-
-    #         # identify which end of the field line strikes the divertor/target that we are considering
-    #         id = (k_divertor, k_target)
-    #         if id == identifiers[1]
-    #             strike_index = 1
-    #         elseif id == identifiers[2]
-    #             strike_index = length(sol1.r)
-    #         else
-    #             @ddtime(target.power_conducted.data = 0.0)
-    #             @ddtime(target.power_convected.data = 0.0)
-    #             @ddtime(target.power_incident.data = 0.0)
-    #             continue
-    #         end
-
-    #         # Power conducted/convected by the plasma on this divertor target  [W]
-    #         @ddtime(target.power_conducted.data = Psol / 2.0)
-    #         @ddtime(target.power_convected.data = 0.0)
-
-    #         # λq at the outer midplane [m]
-    #         resize!(target.two_point_model)
-    #         target.two_point_model[].sol_heat_decay_length = λ_omp
-
-    #         # target flux expansion [-] and wetted area [m²]
-    #         flxexp = @ddtime(target.flux_expansion.data = sol1.total_flux_expansion[strike_index])
-    #         λ_target = flxexp * λ_omp
-    #         wetted_area = @ddtime(target.wetted_area.data = λ_target * 2π * sol1.r[strike_index])
-
-    #         # strike angles [rad]
-    #         @ddtime(target.tilt_angle_tor.data = atan(sol1.Bp[strike_index] / sol1.Bt[strike_index]))
-    #         @ddtime(target.tilt_angle_pol.data = sol1.strike_angles[strike_index == 1 ? 1 : 2])
-
-    #         # Setup model based on dd and run
-    #         boundary_plasma_model = BoundaryPlasmaModels.SOLBoundaryModel(par.boundary_model)
-    #         push!(actor.boundary_plasma_models, boundary_plasma_model)
-    #         BoundaryPlasmaModels.setup_model(boundary_plasma_model, target, eqt, cp1d,cs, sol1; par.impurities, par.impurities_fraction, par.heat_spread_factor,strike_index)
-    #         boundary_plasma_model() # run
-    #         if par.verbose
-    #             println("      == $(uppercase(target.name)) ==")
-    #             BoundaryPlasmaModels.summary(boundary_plasma_model)
-    #             println()
-    #         end
-
-    #         # get the peak power heatflux [W.m²]
-    #         power_flux_peak = @ddtime(target.power_flux_peak.data = boundary_plasma_model.results.q_perp_target_spread)
-
-    #         # get the total power on the divertor [W]
-    #         power_incident = power_flux_peak * wetted_area
-    #         @ddtime(target.power_incident.data = power_incident)
-    #     end
-
-    #     # update totals
-    #     IMAS.divertor_totals_from_targets!(divertor)
-
-    #     @ddtime(divertor.power_thermal_extracted.data = par.thermal_power_extraction_efficiency * @ddtime(divertor.power_incident.data))
-    # end
-
-    return actor
-end
-=======
     end
     populate_divertor_targets_from_sol!(dd, sol1, λ_omp, actor.boundary_plasma_models, Psol, par, strike_indices)
     return actor
@@ -202,4 +127,3 @@ end
         return nothing
     end
     
->>>>>>> Stashed changes
