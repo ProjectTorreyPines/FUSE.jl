14dc9dafe8e18761c8e70e3bf030953152b38921
Brendan Lyons <lyonsbc@fusion.gat.com>
2026-01-26 13:26:31 -0800

    Revert "Allow v1.12 (#1012)"
    This reverts commit b54a3f7c185d09818773f0059b6172526937d8ab.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,35 @@
-println("TEST: using FUSE")
-@time using FUSE
-@time dd = IMAS.dd()
-@time FUSE.warmup(dd)
-println(FUSE.timer)
+# Check if specific test files are requested via ARGS
+if !isempty(ARGS)
+    for testfile in ARGS
+        @info "Running test file: $testfile"
+        include(testfile)
+    end
+else
+    # Default behavior: run all tests
+
+    # Conditionally run extension tests if environment variable is set.
+    if get(ENV, "FUSE_WITH_EXTENSIONS", "false") == "true"
+        using Pkg
+        Pkg.add("ThermalSystemModels")
+        using ThermalSystemModels
+    end
+
+    include("runtests_warmup.jl")
+
+    include("runtests_basics.jl")
+
+    include("runtests_cases.jl")
+
+    include("runtests_actors.jl")
+
+    include("runtests_init_expressions.jl")
+
+    # Conditionally run extension tests if environment variable is set.
+    if get(ENV, "FUSE_WITH_EXTENSIONS", "false") == "true"
+        include("runtests_study.jl")
+    end
+
+    include("runtests_study_database.jl")
+    
+    println(FUSE.timer)
+end
