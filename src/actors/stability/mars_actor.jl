7a9cd911333a1bf66d5bab1766640dbc4a4192b5
akcayc <akcayc@fusion.gat.com>
2026-03-05 14:56:30 -0800

    Reintroduced the coding about Itor, Btor signs to make FF' > 0. However, I may just need to use Abs() instead

--- src/actors/stability/mars_actor.jl
+++ src/actors/stability/mars_actor.jl
@@ -430,8 +430,6 @@ function write_EXPEQ_file(dd::IMAS.dd, par, time_slice_index::Int=1)
 
     # initialize eqt from pulse_schedule and core_profiles
     time_slice = dd.equilibrium.time_slice[time_slice_index]
-    psi0 = time_slice.global_quantities.psi_axis
-    psib = time_slice.global_quantities.psi_boundary
     eqt1d = time_slice.profiles_1d
     
     # populate the input file lines
@@ -496,6 +494,16 @@ function write_EXPEQ_file(dd::IMAS.dd, par, time_slice_index::Int=1)
     pressure_sep_norm = pressure_sep / (Bt_center^2 / μ_0)
     j_tor_norm = j_tor_of_s / (abs(Bt_center) / (r_center * μ_0))
     dpressure_ds = pprime_of_s * (r_center^2 * abs(Bt_center)) / (Bt_center^2 / μ_0)
+
+    # I suspect this logic is to make the q profile > 0, but NOT sure
+    ip_sign = sign(Ip)
+    bt_sign = sign(Bt_center)
+    println("Ip sign: $ip_sign, Bt sign: $bt_sign")
+    ## **** This logic is NOT needed in Martian CHEASE **** ##
+    if (ip_sign == -1 && bt_sign == 1) || (ip_sign == 1 && bt_sign == -1)
+        j_tor_norm .*= -1
+    end
+
 
     # Remove/smooth the X-point along the boundary
     ab = sqrt((maximum(r_bound) - minimum(r_bound))^2 + (maximum(z_bound) - minimum(z_bound))^2) / 2.0
