bd45699ccdf5650d27dfcc193ca06f6d8c61667f
akcayc <akcayc@fusion.gat.com>
2026-05-19 12:47:56 -0700

    Not sure what was changed

--- test/runtests_actors.jl
+++ test/runtests_actors.jl
@@ -1,20 +1,20 @@
 using FUSE
 using Test
 
-@testset "fluxmatcher" begin
-    ini, act = FUSE.case_parameters(:ITER; init_from=:scalars)
-    dd = IMAS.dd()
-    FUSE.init(dd, ini, act)
-    act.ActorFluxMatcher.max_iterations = 2
-    act.ActorFluxMatcher.evolve_pedestal = true
+# @testset "fluxmatcher" begin
+#     ini, act = FUSE.case_parameters(:ITER; init_from=:scalars)
+#     dd = IMAS.dd()
+#     FUSE.init(dd, ini, act)
+#     act.ActorFluxMatcher.max_iterations = 2
+#     act.ActorFluxMatcher.evolve_pedestal = true
 
-    act.ActorFluxMatcher.algorithm = :simple
-    act.ActorTGLF.model = :TJLF
-    FUSE.ActorFluxMatcher(dd, act)
-    act.ActorFluxMatcher.algorithm = :anderson
-    act.ActorTGLF.model = :TGLFNN
-    FUSE.ActorFluxMatcher(dd, act)
-end
+#     act.ActorFluxMatcher.algorithm = :simple
+#     act.ActorTGLF.model = :TJLF
+#     FUSE.ActorFluxMatcher(dd, act)
+#     act.ActorFluxMatcher.algorithm = :anderson
+#     act.ActorTGLF.model = :TGLFNN
+#     FUSE.ActorFluxMatcher(dd, act)
+# end
 
 @testset "Mars Actor CHEASE run" begin
     ini, act = FUSE.case_parameters(:D3D, :default)
@@ -26,7 +26,8 @@ end
     act.ActorMars.restart_equilibrium = false
     act.ActorMars.run_MHD = false
     act.ActorMars.run_mode = :local
-    #act.ActorMars.chease_exec = "/fusion/projects/codes/mars/CHEASE/chease.x"  # <-- Update this path to your CHEASE executable
+    act.ActorMars.chease_exec = "/Users/akcay/Codes/MarsQ_package/CheaseMerge/chease.x"
+    #"/fusion/projects/codes/mars/CHEASE/chease.x"  # <-- Update this path to your CHEASE executable
 
     # Configure CHEASE parameters for testing
     chease_overrides = (NPSI=64, NVEXP=2, NCSCAL=4)
@@ -54,32 +55,29 @@ end
                     @test isfile("OUTRMAR") || error("CHEASE output file OUTPMARS not found.")
                 end
 
-                # test RW surface generation
-                act.ActorMars.number_surfaces = 2
-                chease_overrides_RW = (CFBAL=2.)
-
-                # next test the restart capability
+                # next test the restart capability and RW set-up
                 @info "=============================================================="
                 @info "    Test 2: CHEASE restart capability with resistive wall & higher beta"
                 @info "=============================================================="
                 act.ActorMars.restart_equilibrium = true
-                FUSE.ActorMars(dd, act; chease_overrides=chease_overrides)
+                act.ActorMars.number_surfaces = 2
+                chease_overrides_RW = (CFBAL=1.5,)
+                FUSE.ActorMars(dd, act; chease_overrides=chease_overrides_RW)
 
                 # test MARS MHD stability run
                 @info "=============================================================="
                 @info "       Test 3: Testing MARS MHD stability run"
                 @info "=============================================================="
                 act.ActorMars.restart_equilibrium = false
+                act.ActorMars.run_equilibrium = false
                 act.ActorMars.run_MHD = true
-                if !isfile("RUN.IN")
-                    @test_skip "MARS input file MARS_INPUT not found after CHEASE run. Skipping MARS test."
-                else
-                    FUSE.ActorMars(dd, act; chease_overrides=chease_overrides)
-                    @testset "MARS output files" begin
-                        @test isfile("log_mars") || error("MARS output file MARS_OUTPUT not found.")
-                        #@test filesize("MARS_OUTPUT") > 0 "MARS output file MARS_OUTPUT is empty."
-                    end
+                
+                FUSE.ActorMars(dd, act; mars_overrides=mars_overrides)
+                @testset "MARS output files" begin
+                    @test isfile("log_mars") || error("MARS output file MARS_OUTPUT not found.")
+                    #@test filesize("MARS_OUTPUT") > 0 "MARS output file MARS_OUTPUT is empty."
                 end
+                
 
             end
         end
